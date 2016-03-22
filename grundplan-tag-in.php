7c69daa3961d92e55176c52f49f09ea0da50c1ab
Martin Kreimann <Martin.Kreimann@googlemail.com>
2016-03-22 08:12:40 +0100

    minor

--- grundplan-tag-in.php
+++ grundplan-tag-in.php
@@ -184,6 +184,7 @@ $VKmax = max(array_keys($Mitarbeiter));
 require 'navigation.php';
 
 //Hier beginnt die Normale Ausgabe.
+echo "\t\t<H1>Grundplan Tagesansicht</H1>\n";
 echo "\t\t<div class=no-image>\n";
 echo "\t\t\t<form id=mandantenformular method=post>\n";
 echo "\t\t\t\t<input type=hidden name=wochentag value=".$Grundplan[$wochentag]["Wochentag"][0].">\n";
