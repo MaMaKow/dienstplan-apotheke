e8ef6798f6165ce16a3044522aa2bfd1f183ebbd
Martin Kreimann <Martin.Kreimann@googlemail.com>
2016-05-08 22:09:55 +0200

    Neufassung der Fehlerprüfung. zeichne-histogramm wurde erleichtert.

--- pruefe-dienstplan.php
+++ pruefe-dienstplan.php
@@ -1,10 +1,20 @@
 <?php
+    //Diese Datei zählt Anwesende, Approbierte, Ware-Menschen,...
+    require_once 'headcount-duty-roster.php';
+
     if (isset($Approbierten_anwesende) and isset($tages_ende)) {
         //Wir überprüfen ob zu jeder Zeit Approbierte anwesend sind.
         foreach ($Approbierten_anwesende as $zeit => $anwesende_approbierte) {
             if ($anwesende_approbierte == 0 and $zeit != $tages_ende) {
+              if (!isset($attendant_error)) {
                 $Fehlermeldung[] = 'Um '.date('H:i', $zeit).' Uhr ist kein Approbierter anwesend.';
-                break 1; //We avoid to flood everything with errors for every 5 minutes in which noone is there.
+                //We avoid to flood everything with errors for every 5 minutes in which noone is there.
+                $attendant_error=true;
+                //break 1;
+              }
+            }
+            else {
+                unset ($attendant_error);
             }
         }
     } else {
@@ -15,10 +25,34 @@
             // TODO: Die tatsächlichen Termine für den Wareneingang wären sinnvoller, als die Öffnungszeiten. ($tages_ende)
         foreach ($Wareneingang_Anwesende as $zeit => $anwesende_wareneingang) {
             if ($anwesende_wareneingang == 0 and $zeit != $tages_ende) {
-                $Fehlermeldung[] = 'Um '.date('H:i', $zeit).' Uhr niemand für den Wareneingang anwesend.';
-                break 1; //We avoid to flood everything with errors for every 5 minutes in which noone is there.
+              if (!isset($attendant_error)) {
+                $Warnmeldung[] = 'Um '.date('H:i', $zeit).' Uhr ist niemand für den Wareneingang anwesend.';
+                //break 1; //We avoid to flood everything with errors for every 5 minutes in which noone is there.
+                $attendant_error=true;
+              }
+            }
+            else {
+                unset ($attendant_error);
             }
         }
     } else {
         echo 'Notwendige Variablen sind nicht gesetzt. Keine Zählung der anwesenden Ware-Menschen.';
+    }
+    if (isset($Anwesende) and isset($tages_ende)) {
+            //Wir überprüfen ob zu jeder Zeit jemand anwesend ist, der den Wareneingang machen kann.
+            // TODO: Die tatsächlichen Termine für den Wareneingang wären sinnvoller, als die Öffnungszeiten. ($tages_ende)
+        foreach ($Anwesende as $zeit => $anwesende) {
+            if ($anwesende < 2 and $zeit != $tages_ende) {
+                if (!isset($attendant_error)) {
+                  $Fehlermeldung[] = 'Um '.date('H:i', $zeit).' Uhr sind weniger als zwei Mitarbeiter anwesend.';
+                  $attendant_error=true;
+                }
+                //break 1; //We avoid to flood everything with errors for every 5 minutes in which noone is there.
+            }
+            else {
+                unset ($attendant_error);
+            }
+        }
+    } else {
+        echo 'Notwendige Variablen sind nicht gesetzt. Keine Zählung der Anwesenden.<br>\n';
     }
--- zeichne-histogramm.php
+++ zeichne-histogramm.php
@@ -7,28 +7,6 @@
 		$faktor_arbeitskraft=6; //Wie viele Packungen schafft ein Mitarbeiter pro Zeiteinheit (?halbe Stunde?)?
 		if (!empty($Dienstplan[$tag]["Dienstbeginn"]))
 		{
-			//Im folgenden Suchen wir die Approbierten, die heute anwesend sind. Sie werden im $Approbierten_plan gespeichert.
-			$Spalten=array_keys($Dienstplan[$tag]);
-			foreach($Dienstplan[$tag]["VK"] as $key => $value)
-			{
-				if ( array_search($value, array_keys($Approbierte_mitarbeiter)) !== false )
-				{
-					foreach($Spalten as $spalte)
-					{
-						$Approbierten_dienstplan[$tag][$spalte][$key]=$Dienstplan[$tag][$spalte][$key];
-					}
-				}
-			}
-			foreach($Dienstplan[$tag]["VK"] as $key => $value)
-			{
-				if ( array_search($value, array_keys($Wareneingang_Mitarbeiter)) !== false )
-				{
-					foreach($Spalten as $spalte)
-					{
-						$Wareneingang_dienstplan[$tag][$spalte][$key]=$Dienstplan[$tag][$spalte][$key];
-					}
-				}
-			}
 
 			$abfrage="SELECT * FROM Öffnungszeiten WHERE Wochentag = ".date('N', strtotime($datum))." AND Mandant = ".$mandant;
 			$ergebnis = mysqli_query($verbindungi, $abfrage) OR die ("Error: $abfrage <br>".mysqli_error($verbindungi));
@@ -49,20 +27,6 @@
 			{
 				for ($dienstzeit=$tages_beginn; $dienstzeit<=$tages_ende; $dienstzeit=$dienstzeit+$zeit_abstand){$Dienstzeiten[]=$dienstzeit;}
 			}
-			if ( isset($Approbierten_dienstplan) )
-			{
-				$Approbierten_dienst_enden=array_map('strtotime', $Approbierten_dienstplan[$tag]["Dienstende"]);
-				$Approbierten_dienst_beginne=array_map('strtotime', $Approbierten_dienstplan[$tag]["Dienstbeginn"]);
-				$Approbierten_mittags_enden=array_map('strtotime', $Approbierten_dienstplan[$tag]["Mittagsende"]);
-				$Approbierten_mittags_beginne=array_map('strtotime', $Approbierten_dienstplan[$tag]["Mittagsbeginn"]);
-			}
-			if ( isset($Wareneingang_dienstplan) )
-			{
-				$Wareneingang_Dienst_Enden=array_map('strtotime', $Wareneingang_dienstplan[$tag]["Dienstende"]);
-				$Wareneingang_Dienst_Beginne=array_map('strtotime', $Wareneingang_dienstplan[$tag]["Dienstbeginn"]);
-				$Wareneingang_Mittags_Enden=array_map('strtotime', $Wareneingang_dienstplan[$tag]["Mittagsende"]);
-				$Wareneingang_Mittags_Beginne=array_map('strtotime', $Wareneingang_dienstplan[$tag]["Mittagsbeginn"]);
-			}
 			$Dienst_enden=array_map('strtotime', $Dienstplan[$tag]["Dienstende"]);
 			$Dienst_beginne=array_map('strtotime', $Dienstplan[$tag]["Dienstbeginn"]);
 			$Mittags_enden=array_map('strtotime', $Dienstplan[$tag]["Mittagsende"]);
@@ -73,35 +37,6 @@
 				//Die folgende Umschreibung von $zeit auf eine globale $dienstzeit ist notwendig, um innerhalb der array-filter Funtion darauf per global zugreifen zu können.
 				global $dienstzeit;
 				$dienstzeit=$zeit;
-				if ( isset($Wareneingang_dienstplan) )
-				{
-					//Wir zählen die Approbierten Mitarbeiter
-					//Die Zahl der Mitarbeiter, die irgendwann heute angefangen haben.
-					$wareneingang_Gekommene=count(array_filter(array_filter($Wareneingang_Dienst_Beginne, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					//Die Anzahl der Mitarbeiter, die noch nicht gegangen sind.
-					$wareneingang_Gegangene=count(array_filter(array_filter($Wareneingang_Dienst_Enden, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					$wareneingang_Mittagende=count(array_filter(array_filter($Wareneingang_Mittags_Beginne, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					$wareneingang_Gemittagte=count(array_filter(array_filter($Wareneingang_Mittags_Enden, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					$wareneingang_Mittagende=$wareneingang_Mittagende-$wareneingang_Gemittagte;
-					$wareneingang_Anwesende=$wareneingang_Gekommene-$wareneingang_Gegangene;
-					$wareneingang_Anwesende=$wareneingang_Anwesende-$wareneingang_Mittagende;
-					$Wareneingang_Anwesende[$dienstzeit]=$wareneingang_Anwesende;
-				}
-				if ( isset($Approbierten_dienstplan) )
-				{
-					//Wir zählen die Approbierten Mitarbeiter
-					//Die Zahl der Mitarbeiter, die irgendwann heute angefangen haben.
-					$approbierten_gekommene=count(array_filter(array_filter($Approbierten_dienst_beginne, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					//Die Anzahl der Mitarbeiter, die noch nicht gegangen sind.
-					$approbierten_gegangene=count(array_filter(array_filter($Approbierten_dienst_enden, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					$approbierten_mittagende=count(array_filter(array_filter($Approbierten_mittags_beginne, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					$approbierten_gemittagte=count(array_filter(array_filter($Approbierten_mittags_enden, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
-					$approbierten_mittagende=$approbierten_mittagende-$approbierten_gemittagte;
-					$approbierten_anwesende=$approbierten_gekommene-$approbierten_gegangene;
-					$approbierten_anwesende=$approbierten_anwesende-$approbierten_mittagende;
-					$Approbierten_anwesende[$dienstzeit]=$approbierten_anwesende;
-				}
-				//Und jetzt noch mal für alle Mitarbeiter
 				$gekommene=count(array_filter(array_filter($Dienst_beginne, function($value) {global $dienstzeit; return $value <= $dienstzeit;}))); //Die Zahl der Mitarbeiter, die irgendwann heute angefangen haben.
 				$gegangene=count(array_filter(array_filter($Dienst_enden, function($value) {global $dienstzeit; return $value <= $dienstzeit;}))); //Die Anzahl der Mitarbeiter, die noch nicht gegangen sind.
 				$mittagende=count(array_filter(array_filter($Mittags_beginne, function($value) {global $dienstzeit; return $value <= $dienstzeit;})));
