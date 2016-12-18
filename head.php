bab2cc8c18a6ab35a65a6321c441ae49b14688a7
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2016-12-18 23:41:08 +0100

    Or we might use another datepicker with less requirements:
    https://github.com/chrishulbert/datepicker/

--- head.php
+++ head.php
@@ -2,6 +2,8 @@
   <meta charset=UTF-8>
   <title><?php echo $config['application_name'];?></title>
   <script type="text/javascript" src="javascript.js" ></script>
+  <link rel="stylesheet" type="text/css" href="datepicker.css" /> 
+  <script type="text/javascript" src="datepicker.js"></script>
   <noscript>Sorry, your browser does not support JavaScript!</noscript>
   <link rel="stylesheet" type="text/css" href="style.css" media="all">
   <link rel="stylesheet" type="text/css" href="print.css" media="print">
