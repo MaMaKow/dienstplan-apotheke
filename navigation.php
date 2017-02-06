118339f2742a4c68563935bfd5cb89e7a589bed6
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-02-06 23:26:50 +0100

    Fun with css, w3c and some datepicker style

--- navigation.php
+++ navigation.php
@@ -8,7 +8,7 @@
             <li style=float:right>
                 <a onclick="toggle_show_administration()">
                     <?php echo $user; ?>&nbsp;
-                    <img src=images/settings.png class="inline_image" alt="settings-button" title="Show settings">
+                    <img src=images/settings.png class="inline-image" alt="settings-button" title="Show settings">
                 </a></li>
         </ul>
         <?php require 'administration-in.php'; ?>
@@ -20,59 +20,59 @@
 //In the following lines we will define buttons for the use in other documents.
 
 $rückwärts_button_img='
-		<button type="submit" class="btn btn-primary no-print" value="" name="submitRückwärts">
-			<i class="icon-user icon-black">
-				<img src=images/backward.png width=32px>
+		<button type="submit" class="btn-primary no-print" value="" name="submitRückwärts">
+			<i class="icon-black">
+				<img src=images/backward.png class=button-image alt="Einen Tag rückwärts">
 			</i>
 			<br>
 			1 Tag Rückwärts
 		</button>';
 $vorwärts_button_img='
-		<button type="submit" class="btn btn-primary no-print" value="" name="submitVorwärts">
-			<i class="icon-user icon-black">
-				<img src=images/foreward.png width=32px>
+		<button type="submit" class="btn-primary no-print" value="" name="submitVorwärts">
+			<i class="icon-black">
+				<img src=images/foreward.png class=button-image alt="Einen Tag vorwärts">
 			</i>
 			<br>
 			1 Tag Vorwärts
 		</button>';
 $rückwärts_button_week_img='
-		<button type="submit" class="btn btn-primary no-print" value="" name="submitWocheRückwärts">
-			<i class="icon-user icon-black">
-				<img src=images/backward.png width=32px>
+		<button type="submit" class="btn-primary no-print" value="" name="submitWocheRückwärts">
+			<i class="icon-black">
+				<img src=images/backward.png class=button-image alt="Eine Woche rückwärts">
 			</i>
 			<br>
 			1 Woche Rückwärts
 		</button>';
 $vorwärts_button_week_img='
-		<button type="submit" class="btn btn-primary no-print" value="" name="submitWocheVorwärts">
-			<i class="icon-user icon-black">
-				<img src=images/foreward.png width=32px>
+		<button type="submit" class="btn-primary no-print" value="" name="submitWocheVorwärts">
+			<i class="icon-black">
+				<img src=images/foreward.png class=button-image alt="Eine Woche Vorwärts">
 			</i>
 			<br>
 			1 Woche Vorwärts
 		</button>';
 $submit_button_img='
-		<button type="submit" id=submit_button_img class="btn btn-primary btn-save no-print" value=Absenden name="submitDienstplan">
-		  <i class="icon-user icon-white">
-				<img src=images/save.png width=32px>
+		<button type="submit" id=submit_button_img class="btn-primary btn-save no-print" value=Absenden name="submitDienstplan">
+		  <i class="icon-white">
+				<img src=images/save.png class=button-image alt="Speichern">
 			</i>
 			<br>
 			Speichern
 		</button>';
 // TODO: The button should be inactive when the approval already was done.
 $submit_approval_button_img='
-		<button type="submit" class="btn btn-secondary no-print" value=Genehmigen name="submit_approval">
-			<i class="icon-user icon-grey">
-				<img src=images/approve.png width=32px>
+		<button type="submit" class="btn-secondary no-print" value=Genehmigen name="submit_approval">
+			<i class="icon-grey">
+				<img src=images/approve.png class=button-image alt="Genehmigen">
 			</i>
 			<br>
 			Genehmigen
 		</button>';
 //		<input type=submit value=Genehmigen name='submit_approval'>\n";
 $submit_disapproval_button_img='
-		<button type="submit" class="btn btn-secondary no-print" value=Ablehnen name="submit_disapproval">
-			<i class="icon-user icon-grey">
-				<img src=images/disapprove.png width=32px>
+		<button type="submit" class="btn-secondary no-print" value=Ablehnen name="submit_disapproval">
+			<i class="icon-grey">
+				<img src=images/disapprove.png class=button-image alt="Ablehnen">
 			</i>
 			<br>
 			Ablehnen
