fbe3f80eaf8a8597299166c53d35c08889193720
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-02-21 22:56:41 +0100

    some cleaning up

--- task-rotation.php
+++ task-rotation.php
@@ -39,20 +39,11 @@ function task_rotation_get_worker($date_unix, $task) {
     //TODO: Are there other tasks, that are rotated between people? Is there a weekly, daily or monthly basis?
     //Setup a table in the database:
     $abfrage = "CREATE TABLE IF NOT EXISTS "
-            . "`apotheke`.`task_rotation_daily` ( "
+            . "`apotheke`.`task_rotation` ( "
             . "`date` DATE NOT NULL , "
             . "`task` VARCHAR(64) NOT NULL , "
             . "`VK` TINYINT NOT NULL , "
             . "PRIMARY KEY (`date`,`task`)) "
-            . "ENGINE = InnoDB;";
-    $ergebnis = mysqli_query_verbose($abfrage);
-    $abfrage = "CREATE TABLE IF NOT EXISTS "
-            . "`apotheke`.`task_rotation_weekly` ( "
-            . "`week` TINYINT NOT NULL , "
-            . "`year` SMALLINT NOT NULL , "
-            . "`task` VARCHAR(64) NOT NULL , "
-            . "`VK` TINYINT NOT NULL , "
-            . "PRIMARY KEY (`week`,`year`,`task`)) "
             . "ENGINE = InnoDB;";
     $ergebnis = mysqli_query_verbose($abfrage);
 
@@ -73,7 +64,9 @@ function task_rotation_get_worker($date_unix, $task) {
 }
 
 /*
- * @return int $rotation_vk a worker for a given day and task.
+ * @param int $date_unix The date as a unix time stamp.
+ * @param string $task The task that is to be rotated.
+ * @return int $rotation_vk A worker for a given day and task.
  */
 
 function task_rotation_set_worker($date_unix, $task) {
@@ -81,104 +74,71 @@ function task_rotation_set_worker($date_unix, $task) {
     reset($Rezeptur_Mitarbeiter);
     $date_sql = date("Y-m-d", $date_unix);
     $task_workers_count = count($Rezeptur_Mitarbeiter);
-    //echo "Anzahl der Rezeptare: " . $task_workers_count . "<br>\n";
-    //echo "\$date_sql: " . $date_sql . "<br>\n";
 
     $abfrage = "SELECT * FROM `task_rotation` WHERE `date` <= '$date_sql' and `task` = '$task' ORDER BY `date` DESC LIMIT 1";
     $ergebnis = mysqli_query_verbose($abfrage);
     $row = mysqli_fetch_object($ergebnis);
-    $last_date = $row->date;
-    //If nobody is stored to do a task. Then we have to decide, whos is up to do it.
-    $last_date_unix = strtotime($last_date);
-    //echo "last_date: $last_date<br>\n";
-    for ($temp_date = strtotime(' +1 day', $last_date_unix); $temp_date <= $date_unix; $temp_date = strtotime(' +1 day', $temp_date)) {
-        //echo "Let us think about " . date("d.m.Y", $temp_date) . "<br>\n";
-        $from_date_sql = date("Y-m-d", strtotime("- $task_workers_count WEEKS SUNDAY", $temp_date));
-        $to_date_sql = date("Y-m-d", strtotime("- 1 WEEKS SUNDAY", $temp_date));
-        //echo "\$from_date_sql: $from_date_sql, \$to_date_sql: $to_date_sql<br>\n";
-        $temp_date_sql = date("Y-m-d", $temp_date);
-        //$abfrage = "SELECT VK FROM `task_rotation` WHERE `date` >= '$from_date_sql' AND `date` < '$date_sql'  GROUP BY VK ORDER BY COUNT(date) ASC, VK DESC LIMIT 1";
-        //$abfrage = "SELECT `mitarbeiter`.`VK`, COUNT(date) FROM `task_rotation` RIGHT JOIN mitarbeiter ON `task_rotation`.`VK` = `mitarbeiter`.`VK` WHERE `mitarbeiter`.`VK` IN (7, 12, 16) GROUP BY `mitarbeiter`.VK ORDER BY `mitarbeiter`.`VK`, COUNT(`task_rotation`.date)";
-        /* $abfrage = "SELECT `mitarbeiter`.`VK`, COUNT(`date`) "
-          . "FROM `task_rotation` RIGHT JOIN `mitarbeiter` "
-          . "ON `task_rotation`.`VK` = `mitarbeiter`.`VK` "
-          . "WHERE `mitarbeiter`.`VK` IN (" . implode(",", array_keys($Rezeptur_Mitarbeiter)) . ") "
-          . "AND ( `task_rotation`.`date` IS NULL "
-          . "OR (`task_rotation`.`date` >= '$from_date_sql' "
-          . "AND `task_rotation`.`date` < '$to_date_sql')) "
-          . "GROUP BY `mitarbeiter`.`VK` "
-          . "ORDER BY COUNT(`task_rotation`.`date`) ASC, `mitarbeiter`.`VK` ASC "
-          . "LIMIT 1";
-         * 
-         */
-        foreach ($Rezeptur_Mitarbeiter as $vk => $name) {
-            $abfrage = "SELECT `VK`, COUNT(`date`) as `count`"
-                    . "FROM `task_rotation` "
-                    . "WHERE `VK` = '$vk' "
-                    . "AND `date` > '$from_date_sql' "
-                    . "AND `date` < '$to_date_sql' "
-                    . "GROUP BY `VK` "
-                    . "ORDER BY COUNT(`date`) ASC, `VK` ASC ";
-
-            //echo "$abfrage<br>\n";
-            $ergebnis = mysqli_query_verbose($abfrage);
-            $row = mysqli_fetch_object($ergebnis);
-            if (!empty($row->count)) {
-                $Rezeptur_Count[$vk] = $row->count;
-            } else {
-                $Rezeptur_Count[$vk] = 0;
-            }
-        }
-        reset($Rezeptur_Mitarbeiter);
-        $next_VK = current(array_keys($Rezeptur_Count, min($Rezeptur_Count)));
-//        echo "\$next_VK: $next_VK<br>\n";
-        if (!empty($next_VK)) {
-            $run_iterator = 0;
-            while (key($Rezeptur_Mitarbeiter) != $next_VK and $run_iterator++ < count($Rezeptur_Mitarbeiter)) {
-                //echo "key(\$Rezeptur_Mitarbeiter): " . key($Rezeptur_Mitarbeiter)."<br>\n";
-                next($Rezeptur_Mitarbeiter);
-            }
-            $rotation_vk = key($Rezeptur_Mitarbeiter); //will be overwritten if not present on thet day because of illnes or holidays
-            //          echo "\$rotation_vk: $rotation_vk<br>\n";
-
-            list($Abwesende, $Urlauber, $Kranke) = db_lesen_abwesenheit($temp_date_sql);
-
-            //echo "<pre>"; var_export($Abwesende); echo "</pre><br>";
-            //In case the person is ill or on holidays, someone else has to take the turn:
-            if (isset($Abwesende) and array_search($rotation_vk, $Abwesende) !== false) {
-                $Standard_rotation_vk = $rotation_vk;
-                if (empty(array_diff(array_keys($Rezeptur_Mitarbeiter), $Abwesende))) {
-                    $rotation_vk = NULL;
-                    continue;
+    if (!empty($row->date)) {
+        $last_date = $row->date;
+        //If nobody is stored to do a task. Then we have to decide, whos is up to do it.
+        $last_date_unix = strtotime($last_date);
+        for ($temp_date = strtotime(' +1 day', $last_date_unix); $temp_date <= $date_unix; $temp_date = strtotime(' +1 day', $temp_date)) {
+            $from_date_sql = date("Y-m-d", strtotime("- $task_workers_count WEEKS SUNDAY", $temp_date));
+            $to_date_sql = date("Y-m-d", strtotime("- 1 WEEKS SUNDAY", $temp_date));
+            $temp_date_sql = date("Y-m-d", $temp_date);
+            foreach ($Rezeptur_Mitarbeiter as $vk => $name) {
+                $abfrage = "SELECT `VK`, COUNT(`date`) as `count`"
+                        . "FROM `task_rotation` "
+                        . "WHERE `VK` = '$vk' "
+                        . "AND `date` > '$from_date_sql' "
+                        . "AND `date` < '$to_date_sql' "
+                        . "GROUP BY `VK` "
+                        . "ORDER BY COUNT(`date`) ASC, `VK` ASC ";
+                $ergebnis = mysqli_query_verbose($abfrage);
+                $row = mysqli_fetch_object($ergebnis);
+                if (!empty($row->count)) {
+                    $Rezeptur_Count[$vk] = $row->count;
+                } else {
+                    $Rezeptur_Count[$vk] = 0;
                 }
-                while (isset($Abwesende) and array_search($rotation_vk, $Abwesende) !== false) {
-                    if (FALSE === next($Rezeptur_Mitarbeiter)) {
-                        reset($Rezeptur_Mitarbeiter);
+            }
+            reset($Rezeptur_Mitarbeiter);
+            $next_VK = current(array_keys($Rezeptur_Count, min($Rezeptur_Count)));
+            if (!empty($next_VK)) {
+                $run_iterator = 0;
+                while (key($Rezeptur_Mitarbeiter) != $next_VK and $run_iterator++ < count($Rezeptur_Mitarbeiter)) {
+                    next($Rezeptur_Mitarbeiter);
+                }
+                $rotation_vk = key($Rezeptur_Mitarbeiter); //will be overwritten if not present on thet day because of illnes or holidays
+
+                list($Abwesende, $Urlauber, $Kranke) = db_lesen_abwesenheit($temp_date_sql);
+
+                //In case the person is ill or on holidays, someone else has to take the turn:
+                if (isset($Abwesende) and array_search($rotation_vk, $Abwesende) !== false) {
+                    $Standard_rotation_vk = $rotation_vk;
+                    if (empty(array_diff(array_keys($Rezeptur_Mitarbeiter), $Abwesende))) {
+                        //There is nobody working:
+                        $rotation_vk = NULL;
+                        continue;
                     }
-                    $rotation_vk = key($Rezeptur_Mitarbeiter); //overwrites previously defined value
-                }
-                //Get beack to the previous position for the next turn:
-                while ($Standard_rotation_vk != key($Rezeptur_Mitarbeiter)) {
-                    if (FALSE === prev($Rezeptur_Mitarbeiter)) {
-                        end($Rezeptur_Mitarbeiter);
+                    while (isset($Abwesende) and array_search($rotation_vk, $Abwesende) !== false) {
+                        if (FALSE === next($Rezeptur_Mitarbeiter)) {
+                            reset($Rezeptur_Mitarbeiter);
+                        }
+                        $rotation_vk = key($Rezeptur_Mitarbeiter); //overwrites previously defined value
                     }
                 }
+                $abfrage = "INSERT INTO `task_rotation` (`task`, `date`, `VK`) VALUES ('$task', '$temp_date_sql', '$rotation_vk')";
+                $ergebnis = mysqli_query_verbose($abfrage);
             }
-            $abfrage = "INSERT INTO `task_rotation` (`task`, `date`, `VK`) VALUES ('$task', '$temp_date_sql', '$rotation_vk')";
-            //echo "$abfrage<br>\n";
-            $ergebnis = mysqli_query_verbose($abfrage);
-        } else {
-            //If there is noone anywhere in the past we just take the first person in the array.
-            $rotation_vk = key($Rezeptur_Mitarbeiter);
         }
-        //echo "end of for loop ";
+        return $rotation_vk;
+    } else {
+        //If there is noone anywhere in the past we just take the first person in the array.
+        $rotation_vk = key($Rezeptur_Mitarbeiter);
+        echo "date: $date_sql";
+        $abfrage = "INSERT INTO `task_rotation` (`task`, `date`, `VK`) VALUES ('$task', '$date_sql', '$rotation_vk')";
+        $ergebnis = mysqli_query_verbose($abfrage);
     }
     return $rotation_vk;
-}
-
-function task_rotation_configure_task($task) {
-    //There needs to be a page to configure existing tasks or to install new tasks.
-    $abfrage = "REPLACE INTO ...";
-    $ergebnis = mysqli_query_verbose($abfrage);
-    return FALSE;
 }
