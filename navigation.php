afabc7e51550985cb2c728309a63200889018f99
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-02-26 14:22:32 +0100

    Move folder images to img

--- navigation.php
+++ navigation.php
@@ -8,7 +8,7 @@
             <li style=float:right>
                 <a onclick="toggle_show_administration()">
                     <?php echo $user; ?>&nbsp;
-                    <img src=images/settings.png class="inline-image" alt="settings-button" title="Show settings">
+                    <img src=img/settings.png class="inline-image" alt="settings-button" title="Show settings">
                 </a></li>
         </ul>
         <?php require 'administration-in.php'; ?>
@@ -19,65 +19,63 @@
 
 //In the following lines we will define buttons for the use in other documents.
 
-$rückwärts_button_img='
-		<button type="submit" class="btn-primary no-print" value="" name="submitRückwärts">
-			<i class="icon-black">
-				<img src=images/backward.png class=button-image alt="Einen Tag rückwärts">
+$rückwärts_button_img="
+		<button type='submit' class='btn-primary no-print' value='' name='submitRückwärts'>
+			<i class='icon-black'>
+				<img src='img/backward.png' class='button-image' alt='Einen Tag rückwärts'>
 			</i>
 			<br>
 			1 Tag Rückwärts
-		</button>';
-$vorwärts_button_img='
-		<button type="submit" class="btn-primary no-print" value="" name="submitVorwärts">
-			<i class="icon-black">
-				<img src=images/foreward.png class=button-image alt="Einen Tag vorwärts">
+		</button>";
+$vorwärts_button_img="
+		<button type='submit' class='btn-primary no-print' value='' name='submitVorwärts'>
+			<i class='icon-black'>
+				<img src='img/foreward.png' class='button-image' alt='Einen Tag vorwärts'>
 			</i>
 			<br>
 			1 Tag Vorwärts
-		</button>';
-$rückwärts_button_week_img='
-		<button type="submit" class="btn-primary no-print" value="" name="submitWocheRückwärts">
-			<i class="icon-black">
-				<img src=images/backward.png class=button-image alt="Eine Woche rückwärts">
+		</button>";
+$rückwärts_button_week_img="
+		<button type='submit' class='btn-primary no-print' value='' name='submitWocheRückwärts'>
+			<i class='icon-black'>
+				<img src='img/backward.png' class='button-image' alt='Eine Woche rückwärts'>
 			</i>
 			<br>
 			1 Woche Rückwärts
-		</button>';
-$vorwärts_button_week_img='
-		<button type="submit" class="btn-primary no-print" value="" name="submitWocheVorwärts">
-			<i class="icon-black">
-				<img src=images/foreward.png class=button-image alt="Eine Woche Vorwärts">
+		</button>";
+$vorwärts_button_week_img="
+		<button type='submit' class='btn-primary no-print' value='' name='submitWocheVorwärts'>
+			<i class='icon-black'>
+				<img src='img/foreward.png' class='button-image' alt='Eine Woche Vorwärts'>
 			</i>
 			<br>
 			1 Woche Vorwärts
-		</button>';
-$submit_button_img='
-		<button type="submit" id=submit_button_img class="btn-primary btn-save no-print" value=Absenden name="submitDienstplan">
-		  <i class="icon-white">
-				<img src=images/save.png class=button-image alt="Speichern">
+		</button>";
+$submit_button_img="
+		<button type='submit' id='submit_button_img' class='btn-primary btn-save no-print' value=Absenden name='submitDienstplan'>
+		  <i class='icon-white'>
+				<img src='img/save.png' class='button-image' alt='Speichern'>
 			</i>
 			<br>
 			Speichern
-		</button>';
+		</button>";
 // TODO: The button should be inactive when the approval already was done.
-$submit_approval_button_img='
-		<button type="submit" class="btn-secondary no-print" value=Genehmigen name="submit_approval">
-			<i class="icon-grey">
-				<img src=images/approve.png class=button-image alt="Genehmigen">
+$submit_approval_button_img="
+		<button type='submit' class='btn-secondary no-print' value='Genehmigen' name='submit_approval'>
+			<i class='icon-grey'>
+				<img src='img/approve.png' class='button-image' alt='Genehmigen'>
 			</i>
 			<br>
 			Genehmigen
-		</button>';
-//		<input type=submit value=Genehmigen name='submit_approval'>\n";
-$submit_disapproval_button_img='
-		<button type="submit" class="btn-secondary no-print" value=Ablehnen name="submit_disapproval">
-			<i class="icon-grey">
-				<img src=images/disapprove.png class=button-image alt="Ablehnen">
+		</button>";
+$submit_disapproval_button_img="
+		<button type='submit' class='btn-secondary no-print' value='Ablehnen' name='submit_disapproval'>
+			<i class='icon-grey'>
+				<img src='img/disapprove.png' class='button-image' alt='Ablehnen'>
 			</i>
 			<br>
 			Ablehnen
-		</button>';
-//	<input type=submit value=Ablehnen name='submit_disapproval'>\n";
+		</button>";
 
 function make_select_employee($auswahl_mitarbeiter) {
     global $Mitarbeiter;
