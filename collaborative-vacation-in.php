d7db1a547bd855df5d54d353406547afd25e74ef
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-03-29 23:46:22 +0200

    Start of with a calendar and the existing absence inside it.

---
+++ collaborative-vacation-in.php
@@ -0,0 +1,5 @@
+<?php 
+require_once "default.php";
+require_once "db-lesen-abwesenheit.php";
+require_once "src/php/collaborative-vacation.php";
+?>
--- src/php/collaborative-vacation.php
+++ src/php/collaborative-vacation.php
@@ -19,10 +19,94 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <style>
+            div{
+                /*border: solid red thin;*/
+            }
+            div.year_container{
+                width: 120em;
+            }
+            div.month_container{
+                font-family: monospace;
+                width: 12em;
+                float: left;
+                /*display: inline-block;*/
+            }
+            p.day_paragraph{
+                border: solid black thin;
+                margin-bottom: 0em;
+                margin-top: 0em;
+                /*-webkit-margin-before: 0em;*/
+            }
+            p.weekday{
+                background-color: transparent;
+            }
+            p.weekend{
+                background-color: lightgray;
+            }
+            span.absent_employee_container{
+                border: solid red thin;
+                float: right;
+                width: 2em;
+            }
+        </style>
     </head>
     <body>
         <?php
-        // put your code here
+        if (isset($_POST["year"])) {
+            $year = filter_input(INPUT_POST, "year", FILTER_SANITIZE_NUMBER_INT);
+        } else {
+            $year = date("Y");
+        }
+
+        $start_date = mktime(0, 0, 0, 1, 1, $year);
+        $current_month = date("n", $start_date);
+        $current_month_name = date("F", $start_date);
+        $current_year = date("Y", $start_date);
+
+        echo "<div class=year_container>";
+        echo $current_year . "<br>\n";
+        echo "<div class=month_container>";
+        echo $current_month_name . "<br>\n";
+        for ($date_unix = $start_date; $date_unix < strtotime("+ 1 year", $start_date); $date_unix += 24 * 60 * 60) {
+            list($Abwesende, $Urlauber, $Kranke) = db_lesen_abwesenheit($date_unix);
+            if ($current_month < date("n", $date_unix)) {
+                $current_month = date("n", $date_unix);
+                $current_month_name = date("F", $date_unix);
+                echo "</div class=month_container>";
+                echo "<div class=month_container>";
+                echo $current_month_name . "<br>\n";
+            }
+            $date_text = date("D d", $date_unix);
+            $current_week_day_number = date("N", $date_unix);
+            if ($current_week_day_number < 6) {
+                $paragraph_weekday_class = "weekday";
+                if (isset($Abwesende)) {
+
+                    $absent_employees_containers = "<span class='absent_employee_container'>";
+                    $absent_employees_containers .= implode(array_keys($Abwesende), "</span><span class='absent_employee_container'>");
+                    $absent_employees_containers .= "</span>";
+                } else {
+                    $absent_employees_containers = "";
+                }
+                echo "<p class='day_paragraph "
+                . $paragraph_weekday_class
+                . "'>"
+                . $date_text
+                . " "
+                . $absent_employees_containers
+                . "</p>\n";
+            } else {
+                $paragraph_weekday_class = "weekend";
+                echo "<p class='day_paragraph "
+                . $paragraph_weekday_class
+                . "'>"
+                . $date_text
+                . "</p>\n";
+            }
+        }
+        echo "</div class=year_container>";
+        echo "</div class=month_container>";
         ?>
     </body>
 </html>
