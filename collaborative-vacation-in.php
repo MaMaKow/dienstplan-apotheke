0ac261cd621f59df2d7f591521965409b3d8700a
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-04-08 23:53:36 +0200

    No delete button in create mode, default values

--- collaborative-vacation-in.php
+++ collaborative-vacation-in.php
@@ -8,8 +8,8 @@ if (isset($_POST['employee_id'])) {
 } elseif (isset($_COOKIE['employee_id'])) {
     $employee_id = filter_input(INPUT_COOKIE, employee_id, FILTER_SANITIZE_NUMBER_INT);
 }
-if (isset($auswahl_mitarbeiter)) {
-    create_cookie("auswahl_mitarbeiter", $auswahl_mitarbeiter, 30);
+if (isset($employee_id)) {
+    create_cookie("employee_id", $employee_id, 30);
 }
 require_once "src/php/collaborative-vacation.php";
 ?>
