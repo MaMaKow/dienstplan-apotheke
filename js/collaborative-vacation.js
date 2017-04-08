7ed735b738190594c2c03d224d64de476508891f
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-04-08 23:44:42 +0200

    drag and drop function for input

--- js/collaborative-vacation.js
+++ js/collaborative-vacation.js
@@ -17,48 +17,148 @@
 
 
 "use strict";
-function insert_form_div(element_mouse_is_over) {
+/*function remove_form_div_on_escape(){
+ 
+ evt = evt || window.event;
+ if (evt.keyCode == 27) {
+ alert("Esc was pressed");
+ }
+ }
+ */
+function highlight_absence_create_start() {
+    var x = event.clientX;
+    var y = event.clientY;
+    var element_mouse_is_over = document.elementFromPoint(x, y);
+    var date_unix_from = element_mouse_is_over.attributes.date_unix.nodeValue;
+    var date_sql_from = element_mouse_is_over.attributes.date_sql.nodeValue;
+    window.highlight_absence_create_from_date_unix = date_unix_from;
+    window.highlight_absence_create_from_date_sql = date_sql_from;
+    element_mouse_is_over.className = element_mouse_is_over.className + " highlight";
+    delete window.highlight_absence_create_intermediate_date_unix;
+    delete window.highlight_absence_create_to_date_unix;
+    draw_style_highlight_absence_create();
+}
+function highlight_absence_create_intermediate() {
+    if (1 == event.buttons) { //Only if the left mouse button is pressed down
+        var x = event.clientX;
+        var y = event.clientY;
+        var element_mouse_is_over = document.elementFromPoint(x, y);
+        if (element_mouse_is_over.attributes.date_unix) {
+            var date_unix_intermediate = element_mouse_is_over.attributes.date_unix.nodeValue;
+        } else if (element_mouse_is_over.parentNode.attributes.date_unix) {
+            var date_unix_intermediate = element_mouse_is_over.parentNode.attributes.date_unix.nodeValue;
+        }
+        window.highlight_absence_create_intermediate_date_unix = date_unix_intermediate;
+        draw_style_highlight_absence_create();
+    }
+}
+function draw_style_highlight_absence_create() {
+    var list_of_day_paragraphs = document.getElementsByClassName("day_paragraph");
+    for (var i = 0; i < list_of_day_paragraphs.length; i++) {
+        var date_unix_current = list_of_day_paragraphs[i].attributes.date_unix.nodeValue;
+        var date_range_min = Math.min(window.highlight_absence_create_intermediate_date_unix, window.highlight_absence_create_from_date_unix);
+        var date_range_max = Math.max(window.highlight_absence_create_intermediate_date_unix, window.highlight_absence_create_from_date_unix);
+        if (date_unix_current <= date_range_max && date_unix_current >= date_range_min) {
+            list_of_day_paragraphs[i].className = list_of_day_paragraphs[i].className + " highlight";
+        } else {
+            list_of_day_paragraphs[i].classList.remove("highlight");
+        }
+
+    }
+
+}
+function highlight_absence_create_end() {
+    var x = event.clientX;
+    var y = event.clientY;
+    var element_mouse_is_over = document.elementFromPoint(x, y);
+    //var date_sql_from = window.highlight_absence_create_from_date_sql;
+    if (element_mouse_is_over.attributes.date_sql) {
+        var date_sql_to = element_mouse_is_over.attributes.date_sql.nodeValue;
+    } else if (element_mouse_is_over.parentNode.attributes.date_unix) {
+        var date_sql_to = element_mouse_is_over.parentNode.attributes.date_sql.nodeValue;
+    }
+
+    window.highlight_absence_create_to_date_sql = date_sql_to;
+    insert_form_div("create");
+}
+
+function insert_form_div(edit_create) {
     var x = event.clientX;
     var y = event.clientY;
     var element_mouse_is_over = document.elementFromPoint(x, y);
     var existing_div = document.getElementById('input_box_div');
+    if ("create" === edit_create && "SPAN" === element_mouse_is_over.tagName) {
+        //Create mode firing together with edit mode -> abort!
+        return false;
+    }
     if (existing_div) {
-        if (!is_descendant(existing_div, element_mouse_is_over) && !is_descendant(element_mouse_is_over, existing_div)) {
-            //console.log('Destroying ' + existing_div);
-            //console.log(element_mouse_is_over + ' is not a child of ' + existing_div);
+        if ("HTML" !== element_mouse_is_over.tagName && !is_descendant(existing_div, element_mouse_is_over)) {
             existing_div.parentNode.removeChild(existing_div);
         } else {
             return false; //Do not remove and rebuild when clicking inside the form.
         }
     }
-    //var prototype = document.getElementById('input_box_prototype');
     var div = document.createElement('div');
     element_mouse_is_over.appendChild(div);
     var rect = element_mouse_is_over.getBoundingClientRect();
     div.style.left = rect.left;
     div.style.top = rect.top;
     div.style.position = 'absolute';
-    div.style.backgroundColor = 'inherit';
+    if ("create" == edit_create) {
+        div.style.backgroundColor = '#B4B4B4';
+    } else {
+        div.style.backgroundColor = 'inherit';
+    }
     div.id = 'input_box_div';
     div.className = 'input_box_div'
     fill_input_box_from_prototype(div);
 }
 function prefill_input_box_form() {
     var input_box_div = document.getElementById('input_box_div');
-    var absence_details = JSON.parse(input_box_div.parentNode.attributes.absence_details.nodeValue);
-    var employee_id_select = document.getElementById('employee_id_select');
-    var employee_id_options = employee_id_select.options;
-    for (var i = 0; i < employee_id_options.length; i++) {
-        if (absence_details.employee_id == employee_id_options[i].value) {
-            employee_id_options[i].selected = true;
+    var absence_details_json = input_box_div.parentNode.attributes.absence_details
+    if (absence_details_json) {
+        //Obviously only exists in edit mode:
+        var absence_details = JSON.parse(absence_details_json.nodeValue);
+        var employee_id_select = document.getElementById('employee_id_select');
+        var employee_id_options = employee_id_select.options;
+        for (var i = 0; i < employee_id_options.length; i++) {
+            if (absence_details.employee_id == employee_id_options[i].value) {
+                employee_id_options[i].selected = true;
+            }
         }
+        document.getElementById('input_box_form_start_date').value = absence_details.start;
+        document.getElementById('input_box_form_end_date').value = absence_details.end;
+        document.getElementById('input_box_form_reason').value = absence_details.reason;
+        //In order to remove the old entry we need the former values
+        document.getElementById('input_box_form_start_date_old').value = absence_details.start;
+        document.getElementById('employee_id_old').value = absence_details.employee_id;
+    } else if (window.highlight_absence_create_from_date_sql && window.highlight_absence_create_to_date_sql) {
+        var employee_id_select = document.getElementById('employee_id_select');
+        var employee_id_options = employee_id_select.options;
+        for (var i = 0; i < employee_id_options.length; i++) {
+            if (employee_id == employee_id_options[i].value) {
+                employee_id_options[i].selected = true;
+            }
+        }
+        var to_date_sql = window.highlight_absence_create_to_date_sql;
+        var from_date_sql = window.highlight_absence_create_from_date_sql;
+        var to_date_unix = Date.parse(to_date_sql);
+        var from_date_unix = Date.parse(from_date_sql);
+        if (from_date_unix > to_date_unix) {
+            document.getElementById('input_box_form_start_date').value = to_date_sql;
+            document.getElementById('input_box_form_end_date').value = from_date_sql;
+        } else {
+            document.getElementById('input_box_form_start_date').value = from_date_sql;
+            document.getElementById('input_box_form_end_date').value = to_date_sql;
+        }
+        //document.getElementById('input_box_form_reason').value = ;
+        //In order to remove the old entry we need the former values
+        //TODO: Check if this works for
+        //$query = "DELETE FROM absence WHERE `employee_id` = '$employee_id_old' AND `start` = '$start_date_old_string'";
+
+        document.getElementById('input_box_form_start_date_old').value = "null";
+        document.getElementById('employee_id_old').value = "null";
     }
-    document.getElementById('input_box_form_start_date').value = absence_details.start;
-    document.getElementById('input_box_form_end_date').value = absence_details.end;
-    document.getElementById('input_box_form_reason').value = absence_details.reason;
-    //In order to remove the old entry we need the former values
-    document.getElementById('input_box_form_start_date_old').value = absence_details.start;
-    document.getElementById('employee_id_old').value = absence_details.employee_id;
 }
 function is_descendant(parent, child) {
     var node = child.parentNode;
