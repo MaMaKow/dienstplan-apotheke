854ae5f7b7ce444813b6426a44b882552b1f160a
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-04-16 21:50:46 +0200

    New database structure

--- src/sql/approval.sql
+++ src/sql/approval.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `approval` (
   `date` date NOT NULL,
-  `state` set('approved','not_yet_approved','disapproved','changed_after_approvel') NOT NULL,
+  `state` set('approved','not_yet_approved','disapproved','changed_after_approval') NOT NULL,
   `branch` int(11) NOT NULL,
   `user` varchar(64) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
