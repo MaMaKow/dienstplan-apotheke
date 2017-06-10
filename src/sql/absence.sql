5e401b8f94876e52a38fd6a25fb41c6e7b41a0b0
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-06-10 16:05:56 +0200

    get database structure from production server

--- src/sql/absence.sql
+++ src/sql/absence.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `absence` (
   `employee_id` tinyint(4) NOT NULL,
-  `reason` varchar(64) NOT NULL,
+  `reason` varchar(64) CHARACTER SET latin1 NOT NULL,
   `start` date NOT NULL,
   `end` date NOT NULL,
   `days` int(11) NOT NULL,
-  `approval` set('approved','not_yet_approved','disapproved','changed_after_approval') NOT NULL DEFAULT 'not_yet_approved',
-  `user` varchar(64) NOT NULL,
+  `approval` set('approved','not_yet_approved','disapproved','changed_after_approval') CHARACTER SET latin1 NOT NULL DEFAULT 'not_yet_approved',
+  `user` varchar(64) CHARACTER SET latin1 NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`employee_id`,`start`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci
--- src/sql/pep_bak.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `pep_bak` (
-  `hash` binary(32) NOT NULL,
-  `Datum` date NOT NULL,
-  `Zeit` time NOT NULL,
-  `Anzahl` int(11) NOT NULL,
-  `Mandant` int(11) DEFAULT '1',
-  PRIMARY KEY (`Datum`,`Zeit`,`Anzahl`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
