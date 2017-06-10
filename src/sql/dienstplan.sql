2ae153bf467a804af959ddace9aaf0bbfaed3680
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-06-10 16:05:56 +0200

    get database structure from production server

--- src/sql/dienstplan.sql
+++ src/sql/dienstplan.sql
@@ -5,10 +5,10 @@ CREATE TABLE `dienstplan` (
   `Dienstende` time DEFAULT NULL,
   `Mittagsbeginn` time DEFAULT NULL,
   `Mittagsende` time DEFAULT NULL,
-  `Kommentar` text,
+  `Kommentar` text CHARACTER SET latin1,
   `Stunden` float DEFAULT NULL,
   `Mandant` int(11) NOT NULL DEFAULT '1',
-  `user` varchar(64) NOT NULL,
+  `user` varchar(64) CHARACTER SET latin1 NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`VK`,`Datum`,`Dienstbeginn`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci
