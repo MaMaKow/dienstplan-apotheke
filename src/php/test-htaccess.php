4d7f414130c4ac642af321681b527dd143056d85
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-06-12 00:18:40 +0200

    .htaccess for hidden folders

--- src/php/test-htaccess.php
+++ src/php/test-htaccess.php
@@ -1,7 +1,13 @@
 <?php
 require_once '../../default.php';
-echo test_folders(array("/tmp/", "/tmp/", "/tmp/"));
+echo test_folders(array("/tmp/", "/config/", "/upload/", "/tests/"));
 
+/*
+ * Call the folder via http:// to see if it is visible
+ * 
+ * @param string $folder the name and position of the folder to call.
+ * @return bool | string TRUE for blocked folders, an error message for visible folders.
+ */
 function secret_folder_is_secure($folder = "/tmp/") {
     $dirname = dirname($_SERVER["PHP_SELF"]);
     //This script lies within /src/php/ so therefore we have to move up by two levels:
@@ -12,19 +18,25 @@ function secret_folder_is_secure($folder = "/tmp/") {
     list($response) = get_headers($filename);
     $response_code = substr($response, strpos($response, " "), (strrpos($response, " ") - strpos($response, " ")));
     if (200 == $response_code) {
-        $error_message = "Error! The directory $filename is world visible. Please make sure that the directory is not accessible by the public!";
+        $error_message = "Warning! The directory <a href='$filename'>$filename</a> seems to be world visible. Please make sure that the directory is not accessible by the public!";
         return $error_message;
     } elseif (403 == $response_code) {
         return TRUE;
+    } elseif (404 == $response_code) {
+        //TODO: This is an error. Some directory is missing. But we do not have any place to report it.
+        return TRUE;
     } else {
-        $error_message = "Error! We could not interpret the result. The server returned: '$response'. Please make sure that the directory is not accessible by the public!";
+        $error_message = "Error! The result could not be interpreted for the directory $filename. The server returned: '$response'. Please make sure that the directory is not accessible by the public!";
         return $error_message;
     }
 }
 
+/*
+ * Public execution function for secret_folder_is_secure
+ */
 function test_folders($Folders) {
     $error_message_html = "<div class=error_container>\n";
-    foreach ($Folders as $folder){
+    foreach ((array) $Folders as $folder){
         $secure = secret_folder_is_secure($folder);
         if (TRUE !== $secure){
             $error_message_html .= build_error_message($secure);
@@ -37,12 +49,11 @@ function test_folders($Folders) {
         return $error_message_html;
     } else {
         return FALSE;    
-        echo "Alles ist gut.";
     }
 }
 
 function build_error_message($error_message_text) {
-    $error_message_html = "<p class=errormsg>" . $error_message_text . "</p>\n";
+    $error_message_html = "\t<div class=warningmsg>" . $error_message_text . "</div>\n";
     return $error_message_html;
 }
 ?>
