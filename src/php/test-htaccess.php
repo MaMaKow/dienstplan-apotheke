03dd56d7deb4c6bf6ef693ec05906830e7d38f20
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-06-12 08:48:54 +0200

    A bit more verbosity

--- src/php/test-htaccess.php
+++ src/php/test-htaccess.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../../default.php';
 echo test_folders(array("/tmp/", "/config/", "/upload/", "/tests/"));
 
@@ -8,6 +9,7 @@ echo test_folders(array("/tmp/", "/config/", "/upload/", "/tests/"));
  * @param string $folder the name and position of the folder to call.
  * @return bool | string TRUE for blocked folders, an error message for visible folders.
  */
+
 function secret_folder_is_secure($folder = "/tmp/") {
     $dirname = dirname($_SERVER["PHP_SELF"]);
     //This script lies within /src/php/ so therefore we have to move up by two levels:
@@ -15,7 +17,7 @@ function secret_folder_is_secure($folder = "/tmp/") {
     $dir_above2 = substr($dirname, 0, strrpos($dir_above1, "/"));
     $filename = "http://" . $_SERVER["HTTP_HOST"] . $dir_above2 . $folder;
 
-    list($response) = get_headers($filename);
+    list($response, $Response_remains) = get_headers($filename);
     $response_code = substr($response, strpos($response, " "), (strrpos($response, " ") - strpos($response, " ")));
     if (200 == $response_code) {
         $error_message = "Warning! The directory <a href='$filename'>$filename</a> seems to be world visible. Please make sure that the directory is not accessible by the public!";
@@ -26,7 +28,11 @@ function secret_folder_is_secure($folder = "/tmp/") {
         //TODO: This is an error. Some directory is missing. But we do not have any place to report it.
         return TRUE;
     } else {
-        $error_message = "Error! The result could not be interpreted for the directory $filename. The server returned: '$response'. Please make sure that the directory is not accessible by the public!";
+        $error_message = "Error! The result could not be interpreted for the directory $filename. The server returned: '$response'. Please make sure that the directory is not accessible by the public!<br>";
+        foreach ($Response_remains as $key => $response_http) {
+            $error_message = $key . ": " . $response_http . "<br>\n";
+                    
+        }
         return $error_message;
     }
 }
@@ -34,21 +40,22 @@ function secret_folder_is_secure($folder = "/tmp/") {
 /*
  * Public execution function for secret_folder_is_secure
  */
+
 function test_folders($Folders) {
     $error_message_html = "<div class=error_container>\n";
-    foreach ((array) $Folders as $folder){
+    foreach ((array) $Folders as $folder) {
         $secure = secret_folder_is_secure($folder);
-        if (TRUE !== $secure){
+        if (TRUE !== $secure) {
             $error_message_html .= build_error_message($secure);
             $error_message_exists = TRUE;
         }
     }
     $error_message_html .= "</div>\n";
-    if($error_message_exists){
+    if ($error_message_exists) {
         require_once get_root_folder() . 'head.php';
         return $error_message_html;
     } else {
-        return FALSE;    
+        return FALSE;
     }
 }
 
@@ -56,4 +63,5 @@ function build_error_message($error_message_text) {
     $error_message_html = "\t<div class=warningmsg>" . $error_message_text . "</div>\n";
     return $error_message_html;
 }
+
 ?>
