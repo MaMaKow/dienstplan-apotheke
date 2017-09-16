bd8e5d030017af3f8c5c8c538c43f0f7bfa0bd38
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2017-09-16 22:45:52 +0200

    Readability

--- src/php/pages/reset_lost_password.php
+++ src/php/pages/reset_lost_password.php
@@ -30,9 +30,35 @@ function lost_password_token_is_valid($employee_id, $token) {
     if (!empty($row->employee_id) and $employee_id === $row->employee_id) {
         return TRUE; //The form is shown
     } else {
-        echo build_warning_messages(gettext("Invalid token"));
+        echo build_warning_messages([gettext("Invalid token")]);
         return FALSE;
     }
+}
+
+function build_lost_password_form($employee_id, $user_name, $token) {
+    global $config;
+    global $Error_message;
+    if (lost_password_token_is_valid($employee_id, $token)) {
+        ?>
+        <div class=centered_form_div>
+            <H1><?= $config['application_name'] ?> </H1>
+            <form action="reset_lost_password.php" method="post">
+                <H2><?= $user_name ?></H2>
+                <input type='hidden' name='employee_id' value='<?= $employee_id ?>'>
+                <input type='hidden' name='token' value='<?= $token ?>'>
+                <input type="password" size="40" name="password" required placeholder="Passwort"><br>
+                <input type="password" size="40" maxlength="250" name="password2" required placeholder="Passwort wiederholen" title="Passwort wiederholen"><br><br>
+                <?php
+                echo build_warning_messages([$Error_message], array());
+                ?>
+                <input type="submit" value="Abschicken">
+            </form>
+        </div>
+
+        <?php
+    } else {
+        //echo build_warning_messages(["Invalid token"], array());
+    } //End of if($show_formular)
 }
 
 if (filter_has_var(INPUT_GET, 'token') and filter_has_var(INPUT_GET, 'employee_id')) {
@@ -47,7 +73,7 @@ if (filter_has_var(INPUT_GET, 'token') and filter_has_var(INPUT_GET, 'employee_i
      * Remove expired tokens:
      */
     mysqli_query_verbose("DELETE FROM `users_lost_password_token` WHERE `time_created` <= NOW() - INTERVAL 1 DAY");
-    $show_formular = TRUE;
+    build_lost_password_form($employee_id, $user_name, $token);
 } elseif (filter_has_var(INPUT_POST, 'employee_id')) {
     $error = FALSE;
     $employee_id = filter_input(INPUT_POST, 'employee_id', FILTER_SANITIZE_NUMBER_INT);
@@ -64,7 +90,7 @@ if (filter_has_var(INPUT_GET, 'token') and filter_has_var(INPUT_GET, 'employee_i
         $error = TRUE;
     }
 
-    //No error, we can update the password in the database.
+//No error, we can update the password in the database.
     if (!$error and lost_password_token_is_valid($employee_id, $token)) {
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
@@ -74,36 +100,16 @@ if (filter_has_var(INPUT_GET, 'token') and filter_has_var(INPUT_GET, 'employee_i
         if ($result) {
             clean_up_after_password_change($employee_id);
             echo gettext('Your password has successfully been changed.'), " <a href='" . PDR_HTTP_SERVER_APPLICATION_PATH . "/src/php/login.php'>" . gettext("Login") . "</a>";
-            $show_formular = FALSE;
         } else {
             error_log('Beim Abspeichern ist leider ein Fehler aufgetreten' . print_r($statement->errorInfo(), TRUE));
-            //$Error_message[] = 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+//$Error_message[] = 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
             $Error_message[] = gettext("There was an error while saving the data.");
-            $show_formular = TRUE;
+            build_lost_password_form($employee_id, $user_name, $token);
         }
     }
+} else {
+    echo build_warning_messages([gettext("Missing input token")]);
 }
-
-if (TRUE === $show_formular and lost_password_token_is_valid($employee_id, $token)) {
-    ?>
-    <div class=centered_form_div>
-        <H1><?= $config['application_name'] ?> </H1>
-        <form action="reset_lost_password.php" method="post">
-            <H2><?= $user_name ?></H2>
-            <input type='hidden' name='employee_id' value='<?= $employee_id ?>'>
-            <input type='hidden' name='token' value='<?= $token ?>'>
-            <input type="password" size="40" name="password" required placeholder="Passwort"><br>
-            <input type="password" size="40" maxlength="250" name="password2" required placeholder="Passwort wiederholen" title="Passwort wiederholen"><br><br>
-            <?php
-            echo build_warning_messages($Error_message, array());
-            ?>
-            <input type="submit" value="Abschicken">
-        </form>
-    </div>
-
-    <?php
-} //End of if($show_formular)
 ?>
-
 </BODY>
 </HTML>
