052186759cf309c4cf90dee10fc746580624f3ff
ux31a <git-ux31a@martin-mandelkow.de>
2017-11-28 21:47:40 +0100

    seems to work, needs testing

--- src/php/pages/collaborative-vacation-input-box.php
+++ src/php/pages/collaborative-vacation-input-box.php
@@ -18,8 +18,10 @@
 require_once "../../../default.php";
 require_once PDR_FILE_SYSTEM_APPLICATION_PATH . "/db-lesen-mitarbeiter.php";
 
-if (filter_has_var(INPUT_GET, 'employee_id')) {
-    $employee_id = filter_input(INPUT_GET, 'employee_id', FILTER_SANITIZE_NUMBER_INT);
+print_debug_variable($_GET);
+
+if (filter_has_var(INPUT_GET, 'absence_details_json')) {
+    //An existing entry will be edited
     $absence_details_json_unsafe = filter_input(INPUT_GET, 'absence_details_json', FILTER_UNSAFE_RAW);
     $Absence_details_unsafe = json_decode($absence_details_json_unsafe, TRUE);
     $filters = array(
@@ -29,7 +31,21 @@ if (filter_has_var(INPUT_GET, 'employee_id')) {
         'end' => FILTER_SANITIZE_STRING,
     );
     $Absence_details = filter_var_array($Absence_details_unsafe, $filters);
-    var_export($Absence_details);
+    $employee_id = $Absence_details['employee_id'];
+} elseif (filter_has_var(INPUT_GET, 'highlight_details_json')) {
+    //A new entry will be created
+    $highlight_details_json_unsafe = filter_input(INPUT_GET, 'highlight_details_json', FILTER_UNSAFE_RAW);
+    $Highlight_details_unsafe = json_decode($highlight_details_json_unsafe, TRUE);
+    $filters = array(
+        'highlight_absence_create_from_date_sql' => FILTER_SANITIZE_STRING,
+        'highlight_absence_create_to_date_sql' => FILTER_SANITIZE_STRING,
+    );
+    $Highlight_details = filter_var_array($Highlight_details_unsafe, $filters);
+    $employee_id = $Highlight_details['employee_id'];
+    print_debug_variable($Highlight_details);
+    $Absence_details['reason'] = gettext("Vacation");
+    $Absence_details['start'] = $Highlight_details['highlight_absence_create_from_date_sql'];
+    $Absence_details['end'] = $Highlight_details['highlight_absence_create_to_date_sql'] ? $Highlight_details['highlight_absence_create_to_date_sql'] : $Highlight_details['highlight_absence_create_from_date_sql'];
 } elseif (filter_has_var(INPUT_COOKIE, 'employee_id')) {
     $employee_id = filter_input(INPUT_COOKIE, 'employee_id', FILTER_SANITIZE_NUMBER_INT);
 } else {
@@ -37,54 +53,56 @@ if (filter_has_var(INPUT_GET, 'employee_id')) {
 }
 ?>
 <form id="input_box_form" method="POST" onmousedown="stop_click_propagation();">
-    <select name="employee_id" id="employee_id_select">
-        <?php
-        if ($session->user_has_privilege('create_absence')) {
-            foreach ($List_of_employees as $employee_id_option => $last_name) {
-                if ($employee_id_option == $employee_id) {
-                    $option_selected = "selected";
-                } else {
-                    $option_selected = "";
-                }
-                echo "\t\t<option id='employee_id_option_$employee_id_option' value='$employee_id_option' $option_selected>";
-                echo "$employee_id_option $last_name";
-                echo "</option>\n";
+    <p><?= gettext("Employee") ?><br><select name="employee_id" id="employee_id_select"></p>
+    <?php
+    if ($session->user_has_privilege('create_absence')) {
+        foreach ($List_of_employees as $employee_id_option => $last_name) {
+            if ($employee_id_option == $employee_id) {
+                $option_selected = "selected";
+            } else {
+                $option_selected = "";
             }
-        } elseif ($session->user_has_privilege('request_own_absence') and "" === $employee_id) {
-            echo "\t\t<option id='employee_id_option_" . $_SESSION['user_employee_id'] . "' value=" . $_SESSION['user_employee_id'] . ">";
-            echo $_SESSION['user_employee_id'] . " " . $List_of_employees[$_SESSION['user_employee_id']];
-            echo "</option>\n";
-        } else {
-            echo "\t\t<option id='employee_id_option_" . $employee_id . "' value=" . $employee_id . ">";
-            echo $employee_id . " " . $List_of_employees[$employee_id];
+            echo "\t\t<option id='employee_id_option_$employee_id_option' value='$employee_id_option' $option_selected>";
+            echo "$employee_id_option $last_name";
             echo "</option>\n";
         }
-        ?>
-    </select>
-    <!--
-    <img src="" style="width: 0" alt=""
-         onerror="prefill_input_box_form(); this.parentNode.removeChild(this);"
-         data-comment="This element is necessary to allow interaction of javascript with this element. After the execution, it is removed."
-         />
-    -->
-    <input type="date" id="input_box_form_start_date" name="start_date" value="<?= $Absence_details['start'] ?>">
-    <input type="date" id="input_box_form_end_date" name="end_date" value="<?= $Absence_details['end'] ?>">
-    <input type="text" id="input_box_form_reason" name="reason" list='reasons' value="<?= $Absence_details['reason'] ?>">
-    <?php
-    if (
-            $session->user_has_privilege('create_absence')
-            or ( $session->user_has_privilege('request_own_absence')
-            and ( $_SESSION['user_employee_id'] === $employee_id
-            or "" === $employee_id)
-            )
-    ) {
-        ?>
-        <button type="submit" value="save" name="command" class="button_tight">Speichern</button>
-        <button type="submit" value="delete" name="command" id="input_box_form_button_delete" class="button_tight">Löschen</button>
-    <?php } ?>
+    } elseif ($session->user_has_privilege('request_own_absence') and "" === $employee_id) {
+        echo "\t\t<option id='employee_id_option_" . $_SESSION['user_employee_id'] . "' value=" . $_SESSION['user_employee_id'] . ">";
+        echo $_SESSION['user_employee_id'] . " " . $List_of_employees[$_SESSION['user_employee_id']];
+        echo "</option>\n";
+    } else {
+        echo "\t\t<option id='employee_id_option_" . $employee_id . "' value=" . $employee_id . ">";
+        echo $employee_id . " " . $List_of_employees[$employee_id];
+        echo "</option>\n";
+    }
+    ?>
+</select>
+<!--
+<img src="" style="width: 0" alt=""
+     onerror="prefill_input_box_form(); this.parentNode.removeChild(this);"
+     data-comment="This element is necessary to allow interaction of javascript with this element. After the execution, it is removed."
+     />
+-->
+<p><?= gettext("Start") ?><br><input type="date" id="input_box_form_start_date" name="start_date" value="<?= $Absence_details['start'] ?>"></p>
+<p><?= gettext("End") ?><br><input type="date" id="input_box_form_end_date" name="end_date" value="<?= $Absence_details['end'] ?>"></p>
+<p><?= gettext("Reason") ?><br><input type="text" id="input_box_form_reason" name="reason" list='reasons' value="<?= $Absence_details['reason'] ?>"></p>
+<?php
+if (
+        $session->user_has_privilege('create_absence')
+        or ( $session->user_has_privilege('request_own_absence')
+        and ( $_SESSION['user_employee_id'] === $employee_id
+        or "" === $employee_id)
+        )
+) {
+    ?>
+    <p>
+        <button type="submit" value="save" name="command" class="button_tight"><?= gettext("Save") ?></button>
+        <button type="submit" value="delete" name="command" id="input_box_form_button_delete" class="button_tight"><?= gettext("Delete") ?></button>
+    </p>
+<?php } ?>
 
-    <input type="hidden" id="employee_id_old" name="employee_id_old" value="<?= $Absence_details['employee_id'] ?>">
-    <input type="hidden" id="input_box_form_start_date_old" name="start_date_old" value="<?= $Absence_details['start'] ?>">
+<input type="hidden" id="employee_id_old" name="employee_id_old" value="<?= $Absence_details['employee_id'] ?>">
+<input type="hidden" id="input_box_form_start_date_old" name="start_date_old" value="<?= $Absence_details['start'] ?>">
 </form>
 <a title="<?= gettext("Close"); ?>" href="#" onclick="remove_form_div()">
     <span id="remove_form_div_span">
