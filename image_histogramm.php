3c469b51228c69cffcc6fbf884133a5b6366d1bd
ux31a <git-ux31a@martin-mandelkow.de>
2017-12-04 11:29:47 +0100

    debug image_histogram, debug grundplan-tag-in weekday form

--- image_histogramm.php
+++ image_histogramm.php
@@ -27,8 +27,8 @@ function check_timeliness_of_pep_data() {
     $seconds_since_last_update = $today - $newest_pep_date;
     if ($seconds_since_last_update >= 60 * 60 * 24 * 30 * 3) { //3 months
         $timeliness_warning_html = "<br><div class=warningmsg>Die PEP Information ist veraltet. <br>"
-        . "Letzter Eintrag " . date('d.m.Y', strtotime($row->Datum)) . ". <br>"
-        . "Bitte neue PEP-Datei <a href=upload-in.php>hochladen</a>!</div><br>\n";
+                . "Letzter Eintrag " . date('d.m.Y', strtotime($row->Datum)) . ". <br>"
+                . "Bitte neue PEP-Datei <a href=upload-in.php>hochladen</a>!</div><br>\n";
         return $timeliness_warning_html;
     }
 }
@@ -37,18 +37,19 @@ function get_Erwartung($datum, $mandant) {
     global $Dienstplan, $Anwesende;
     require_once 'headcount-duty-roster.php';
     if (basename($_SERVER["SCRIPT_FILENAME"]) === 'tag-in.php') {
-        echo check_timeliness_of_pep_data($param);
+        echo check_timeliness_of_pep_data();
     }
-
-    global $verbindungi;
-    global $Branch_pep_id;
 
     $unix_datum = strtotime($datum);
     $sql_weekday = date('N', $unix_datum) - 1;
     $month_day = date('j', $unix_datum);
     $month = date('n', $unix_datum);
 
+    global $Branch_pep_id;
     $branch_pep_id = $Branch_pep_id[$mandant];
+    if (empty($branch_pep_id)) {
+        return FALSE;
+    }
 
     $sql_query = "SELECT Uhrzeit, Mittelwert FROM `pep_weekday_time`  WHERE Mandant = $branch_pep_id and Wochentag = $sql_weekday";
     $result = mysqli_query_verbose($sql_query);
@@ -74,9 +75,8 @@ function get_Erwartung($datum, $mandant) {
     return $Erwartung;
 }
 
-
 /**
- * 
+ *
  * @param array $Dienstplan
  * @global int $mandant
  * @global string $datum
@@ -88,7 +88,9 @@ function draw_image_histogramm($Dienstplan) {
     global $mandant, $Anwesende, $datum;
     global $factor_employee;
     $factor_employee = 6;
-
+    if (empty($Anwesende)) {
+        return FALSE;
+    }
 //TODO: Erwartung should be moved into the databasecompletely!
 // We need the reader for Erwartung here or inside a seperate function file!
     $Erwartung = get_Erwartung($datum, $mandant);
