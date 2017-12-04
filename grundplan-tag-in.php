19c2353efe7afbfd3fe79ff81207b1836dd19657
ux31a <git-ux31a@martin-mandelkow.de>
2017-12-04 11:29:47 +0100

    debug image_histogram, debug grundplan-tag-in weekday form

--- grundplan-tag-in.php
+++ grundplan-tag-in.php
@@ -179,7 +179,7 @@ echo build_select_branch($mandant, $date_sql);
 
 //Auswahl des Wochentages
 echo "\t\t\t<form id=wochentagformular method=post>\n";
-echo "\t\t\t\t<input type=hidden name=mandant value=" . htmlentities($Grundplan[$wochentag]["Mandant"][0]) . ">\n";
+echo "\t\t\t\t<input type=hidden name=mandant value=" . $mandant . ">\n";
 echo "\t\t\t\t<select class='no-print large' name=wochentag onchange=this.form.submit()>\n";
 //echo "\t\t\t\t\t<option value=".$wochentag.">".$Wochentage[$wochentag]."</option>\n";
 foreach ($Wochentage as $temp_weekday => $value) {
