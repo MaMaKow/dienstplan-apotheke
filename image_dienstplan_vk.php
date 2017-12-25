0bb7e68b7ac3a6344d5e82a113f8ce1e5d5cd98b
ux31a <git-ux31a@martin-mandelkow.de>
2017-12-25 13:36:02 +0100

    same-day multi branch support for principle roster plot

--- image_dienstplan_vk.php
+++ image_dienstplan_vk.php
@@ -26,7 +26,7 @@
 function draw_image_dienstplan_vk($Dienstplan) {
     global $List_of_employees, $List_of_employee_professions;
 
-    $line = 0; //TODO: This will be hardcoded here. It could be calculated in a later use case.
+    //$line = 0; //TODO: This will be hardcoded here. It could be calculated in a later use case.
 
 
     $bar_height = 20;
@@ -48,11 +48,15 @@ function draw_image_dienstplan_vk($Dienstplan) {
     $svg_outer_height = $svg_inner_height + ($outer_margin_y * 2);
 
     foreach ($Dienstplan as $day => $Column) {
-        if (NULL !== $Dienstplan[$day]['Dienstende'][$line]) {
-            $day_ends[] = $Dienstplan[$day]['Dienstende'][$line];
+        foreach ($Dienstplan[$day]['Dienstende'] as $row => $row_value) {
+            if (NULL !== $row_value) {
+                $day_ends[] = $row_value;
+            }
         }
-        if (NULL !== $Dienstplan[$day]['Dienstbeginn'][$line]) {
-            $day_starts[] = $Dienstplan[$day]['Dienstbeginn'][$line];
+        foreach ($Dienstplan[$day]['Dienstbeginn'] as $row => $row_value) {
+            if (NULL !== $row_value) {
+                $day_starts[] = $row_value;
+            }
         }
     }
     $first_start = min($day_starts);
@@ -87,61 +91,64 @@ function draw_image_dienstplan_vk($Dienstplan) {
     $svg_break_box_text = '';
 
     foreach ($Dienstplan as $day => $Column) {
+        foreach ($Dienstplan[$day]['Dienstbeginn'] as $row => $row_value) {
+            print_debug_variable($row_value);
 //        echo "<pre>"; var_dump(time_from_text_to_int($Dienstplan[$day]['Dienstbeginn'][$line])); echo "</pre>";
-        if (NULL === $Dienstplan[$day]['Dienstbeginn'][$line]) {
-            continue;
+            if (NULL === $Dienstplan[$day]['Dienstbeginn'][$row]) {
+                continue;
+            }
+            $vk = $Dienstplan[$day]['VK'][$row];
+
+            $dienst_beginn = time_from_text_to_int($Dienstplan[$day]['Dienstbeginn'][$row]);
+            $dienst_ende = time_from_text_to_int($Dienstplan[$day]['Dienstende'][$row]);
+            $break_start = time_from_text_to_int($Dienstplan[$day]['Mittagsbeginn'][$row]);
+            $break_end = time_from_text_to_int($Dienstplan[$day]['Mittagsende'][$row]);
+            $working_hours = $Dienstplan[$day]['Stunden'][$row];
+            $width_in_hours = $dienst_ende - $dienst_beginn;
+            $break_width_in_hours = $break_end - $break_start;
+
+            //The next lines will be used for coloring the image dependent on the education of the workers:
+            if ($List_of_employee_professions[$vk] == "Apotheker") {
+                $worker_style = 1;
+            } elseif ($List_of_employee_professions[$vk] == "PI") {
+                $worker_style = 1;
+            } elseif ($List_of_employee_professions[$vk] == "PTA") {
+                $worker_style = 2;
+            } elseif ($List_of_employee_professions[$vk] == "PKA") {
+                $worker_style = 3;
+            } else {
+                //anybody else
+                $worker_style = 3;
+            }
+
+
+
+
+            // echo "$dienst_beginn $first_start<br>\n";
+            $x_pos_box = $outer_margin_x + $inner_margin_x + ($dienst_beginn - floor($first_start)) * $bar_width_factor;
+            $x_pos_break_box = $x_pos_box + (($break_start - $dienst_beginn) * $bar_width_factor);
+            $x_pos_text = $x_pos_box;
+            $y_pos_box = $outer_margin_y + ($inner_margin_y * ($day)) + ($bar_height * ($day - 1));
+            $y_pos_text = $y_pos_box + $bar_height;
+            $width = $width_in_hours * $bar_width_factor;
+            $break_width = $break_width_in_hours * $bar_width_factor;
+            $x_pos_text_secondary = $x_pos_text + $width;
+            if (basename($_SERVER["SCRIPT_FILENAME"]) === 'tag-in.php') {
+                $cursor_style_box = 'move';
+                $cursor_style_break_box = 'cell';
+            } else {
+                $cursor_style_box = 'default';
+                $cursor_style_break_box = 'default';
+            }
+
+            $svg_box_text .= "<g id=work_box_$day transform='matrix(1 0 0 1 0 0)' onmousedown='selectElement(evt, \"group\")' >";
+            $svg_box_text .= "\t<rect x='$x_pos_box' y='$y_pos_box' width='$width' height='$bar_height' style='fill: $Worker_style[$worker_style];cursor: $cursor_style_box;' />\n";
+            $svg_box_text .= "\t\t<text x='$x_pos_text' y='$y_pos_text' font-family='sans-serif' font-size='$font_size' alignment-baseline='ideographic'>" . $List_of_employees[$vk] . "</text>\n";
+            $svg_box_text .= "\t\t<text x='$x_pos_text_secondary' y='$y_pos_text' font-family='sans-serif' font-size='$font_size' alignment-baseline='ideographic' text-anchor='end'>" . $working_hours . "</text>\n";
+            $svg_box_text .= "</g>";
+
+            $svg_box_text .= "\t<rect id=break_box_$day transform='matrix(1 0 0 1 0 0)' onmousedown='selectElement(evt, \"single\")' x='$x_pos_break_box' y='$y_pos_box' width='$break_width' height='$bar_height' stroke='black' stroke-width='0.3' style='fill:#FEFEFF; cursor: $cursor_style_break_box;' />\n";
         }
-        $vk = $Dienstplan[$day]['VK'][$line];
-
-        $dienst_beginn = time_from_text_to_int($Dienstplan[$day]['Dienstbeginn'][$line]);
-        $dienst_ende = time_from_text_to_int($Dienstplan[$day]['Dienstende'][$line]);
-        $break_start = time_from_text_to_int($Dienstplan[$day]['Mittagsbeginn'][$line]);
-        $break_end = time_from_text_to_int($Dienstplan[$day]['Mittagsende'][$line]);
-        $working_hours = $Dienstplan[$day]['Stunden'][$line];
-        $width_in_hours = $dienst_ende - $dienst_beginn;
-        $break_width_in_hours = $break_end - $break_start;
-
-        //The next lines will be used for coloring the image dependent on the education of the workers:
-        if ($List_of_employee_professions[$vk] == "Apotheker") {
-            $worker_style = 1;
-        } elseif ($List_of_employee_professions[$vk] == "PI") {
-            $worker_style = 1;
-        } elseif ($List_of_employee_professions[$vk] == "PTA") {
-            $worker_style = 2;
-        } elseif ($List_of_employee_professions[$vk] == "PKA") {
-            $worker_style = 3;
-        } else {
-            //anybody else
-            $worker_style = 3;
-        }
-
-
-
-
-        // echo "$dienst_beginn $first_start<br>\n";
-        $x_pos_box = $outer_margin_x + $inner_margin_x + ($dienst_beginn - floor($first_start)) * $bar_width_factor;
-        $x_pos_break_box = $x_pos_box + (($break_start - $dienst_beginn) * $bar_width_factor);
-        $x_pos_text = $x_pos_box;
-        $y_pos_box = $outer_margin_y + ($inner_margin_y * ($day)) + ($bar_height * ($day - 1));
-        $y_pos_text = $y_pos_box + $bar_height;
-        $width = $width_in_hours * $bar_width_factor;
-        $break_width = $break_width_in_hours * $bar_width_factor;
-        $x_pos_text_secondary = $x_pos_text + $width;
-        if (basename($_SERVER["SCRIPT_FILENAME"]) === 'tag-in.php') {
-            $cursor_style_box = 'move';
-            $cursor_style_break_box = 'cell';
-        } else {
-            $cursor_style_box = 'default';
-            $cursor_style_break_box = 'default';
-        }
-
-        $svg_box_text .= "<g id=work_box_$day transform='matrix(1 0 0 1 0 0)' onmousedown='selectElement(evt, \"group\")' >";
-        $svg_box_text .= "\t<rect x='$x_pos_box' y='$y_pos_box' width='$width' height='$bar_height' style='fill: $Worker_style[$worker_style];cursor: $cursor_style_box;' />\n";
-        $svg_box_text .= "\t\t<text x='$x_pos_text' y='$y_pos_text' font-family='sans-serif' font-size='$font_size' alignment-baseline='ideographic'>" . $List_of_employees[$vk] . "</text>\n";
-        $svg_box_text .= "\t\t<text x='$x_pos_text_secondary' y='$y_pos_text' font-family='sans-serif' font-size='$font_size' alignment-baseline='ideographic' text-anchor='end'>" . $working_hours . "</text>\n";
-        $svg_box_text .= "</g>";
-
-        $svg_box_text .= "\t<rect id=break_box_$day transform='matrix(1 0 0 1 0 0)' onmousedown='selectElement(evt, \"single\")' x='$x_pos_break_box' y='$y_pos_box' width='$break_width' height='$bar_height' stroke='black' stroke-width='0.3' style='fill:#FEFEFF; cursor: $cursor_style_break_box;' />\n";
     }
     $svg_text .= $svg_box_text;
     $svg_text .= $svg_grid_text;
