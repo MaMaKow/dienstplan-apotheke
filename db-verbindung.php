51ac961b0b4b9df2ded05784955d8f164902221a
ux31a <git-ux31a@martin-mandelkow.de>
2018-01-24 09:47:05 +0100

    refactor database tables

--- db-verbindung.php
+++ db-verbindung.php
@@ -60,3 +60,26 @@ function mysqli_query_verbose($sql_query, $inside_transaction = FALSE) {
     }
     return $result;
 }
+
+/**
+ * Check if a table exists in the current database.
+ *
+ * https://stackoverflow.com/a/14355475/2323627
+ *
+ * @param PDO $pdo PDO instance connected to a database.
+ * @param string $table Table to search for.
+ * @return bool TRUE if table exists, FALSE if no table found.
+ */
+function pdr_database_table_exists($table_name) {
+    // Try a select statement against the table
+    // Run it in try/catch in case PDO is in ERRMODE_EXCEPTION.
+    global $pdo;
+    try {
+        $result = $pdo->query("SELECT 1 FROM $table LIMIT 1");
+    } catch (Exception $e) {
+        // We got an exception == table not found
+        return FALSE;
+    }
+    // Result is either boolean FALSE (no table found) or PDOStatement Object (table found)
+    return $result !== FALSE;
+}
