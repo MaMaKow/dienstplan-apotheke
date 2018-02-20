9d53a8900c26d728e2bf165dedc79eb815e6ebf2
ux31a <git-ux31a@martin-mandelkow.de>
2018-02-20 15:19:35 +0100

    debugging and inline documentation

--- src/php/classes/class.branch.php
+++ src/php/classes/class.branch.php
@@ -1,21 +1,32 @@
 <?php
 
-class branch {
-    /*
-     * TODO: Make an object class branch!
-     * Make an array of branch objects!
-     */
+/*
+ * Pharmacies and connected branches
+ *
+ * In German law one Pharmacist is allowed to own one main pharmacy and three branches.
+ * Each branch is led by one pharmacist branch manager.
+ *
+ * It is possible to create unlimited theoretical branches like "Field service", "Compounding" or "Hospital" to pricisely define, who is working where and for which task at which time.
+ *
+ * @author Dr. Martin Mandelkow
+ */
 
-    public $List_of_branches;
+class branch {
+
     public $name;
     public $short_name;
     public $address;
     public $manager;
     public $PEP;
 
+    /*
+     * read the branch data from the database
+     * @return array An array ob objects of the class branch
+     */
+
     static function read_branches_from_database() {
-        //Get a list of branches:
         /*
+         * Get a list of branches:
          * CAVE! This function is thought to be called from the outside of this class only.
          */
         if (!is_null($this)) {
@@ -41,23 +52,28 @@ class branch {
         }
 
         if (empty(array_keys($Branches))) {
-            /*
-             * TODO: redirect to the branch-management form if the script is not the branch-management form.
-             * Also Show an error-message/hint in the branch-management, that no branch is existing.
-             * TODO: Make this a function!
-             * TODO: Return a single branch-array with allthe information.
-             * Or learn to even make this an object.
-             */
-            $script_name = filter_input(INPUT_SERVER, "SCRIPT_NAME", FILTER_SANITIZE_STRING);
-            $request_uri = filter_input(INPUT_SERVER, "REQUEST_URI", FILTER_SANITIZE_URL);
-
-            if (!in_array(basename($script_name), array('branch-management.php'))) {
-                $location = PDR_HTTP_SERVER_APPLICATION_PATH . "src/php/pages/branch-management.php";
-                header("Location:" . $location . "?referrer=" . $request_uri);
-                die('<p><a href="' . $location . '?referrer=' . $request_uri . '">Please configure at least one branch first!</a></p>');
-            }
+            $this->redirect_to_input_form_on_missing_setup();
         } else {
             return $Branches;
+        }
+    }
+
+    /*
+     * redirect the browser to the branch management form
+     *
+     * If no branch is setup yet (e.g, directly after installation, or if all the branches have been deleted)
+     * then the browser is redirected to the branch management form
+     * @return void
+     */
+
+    private static function redirect_to_input_form_on_missing_setup() {
+
+        if (!in_array(basename($script_name), array('branch-management.php'))) {
+            $script_name = filter_input(INPUT_SERVER, 'SCRIPT_NAME', FILTER_SANITIZE_STRING);
+            $request_uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL);
+            $location = PDR_HTTP_SERVER_APPLICATION_PATH . 'src/php/pages/branch-management.php';
+            header('Location:' . $location);
+            die('<p><a href="' . $location . '>Please configure at least one branch first!</a></p>');
         }
     }
 
