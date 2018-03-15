00de4f0dd8550ade01a9ea9b3b8844e4c57e6b0a
ux31a <git-ux31a@martin-mandelkow.de>
2018-03-15 20:49:03 +0100

    debug image histogram

--- image_histogramm.php
+++ image_histogramm.php
@@ -17,59 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-function check_timeliness_of_pep_data() {
-    //Check if the PEP information is still up-to-date:
-    $sql_query = "SELECT max(Datum) as Datum FROM `pep`";
-    $result = mysqli_query_verbose($sql_query);
-    $row = mysqli_fetch_object($result);
-    $newest_pep_date = strtotime($row->Datum);
-    $today = time();
-    $seconds_since_last_update = $today - $newest_pep_date;
-    if ($seconds_since_last_update >= 60 * 60 * 24 * 30 * 3) { //3 months
-        $timeliness_warning_html = "<br><div class=warningmsg>Die PEP Information ist veraltet. <br>"
-                . "Letzter Eintrag " . date('d.m.Y', strtotime($row->Datum)) . ". <br>"
-                . "Bitte neue PEP-Datei <a href=upload-in.php>hochladen</a>!</div><br>\n";
-        return $timeliness_warning_html;
-    }
-}
-
-function get_Erwartung($datum, $branch_id) {
-    global $List_of_branch_objects;
-    require_once 'headcount-duty-roster.php';
-    if (basename($_SERVER["SCRIPT_FILENAME"]) === 'tag-in.php') {
-        echo check_timeliness_of_pep_data();
-    }
-
-    $unix_datum = strtotime($datum);
-    $sql_weekday = date('N', $unix_datum) - 1;
-    $month_day = date('j', $unix_datum);
-    $month = date('n', $unix_datum);
-
-    $branch_pep_id = $List_of_branch_objects[$branch_id]->PEP;
-    if (empty($branch_pep_id)) {
-        return FALSE;
-    }
-
-    $result = mysqli_query_verbose("SELECT Uhrzeit, Mittelwert FROM `pep_weekday_time`  WHERE Mandant = $branch_pep_id and Wochentag = $sql_weekday");
-    while ($row = mysqli_fetch_object($result)) {
-        $Packungen[$row->Uhrzeit] = $row->Mittelwert;
-    }
-    $result = mysqli_query_verbose("SELECT factor FROM `pep_month_day`  WHERE `branch` = $branch_pep_id and `day` = $month_day");
-    $row = mysqli_fetch_object($result);
-    $factor_tag_im_monat = $row->factor;
-
-    $result = mysqli_query_verbose("SELECT factor FROM `pep_year_month`  WHERE `branch` = $branch_pep_id and `month` = $month");
-    $row = mysqli_fetch_object($result);
-    $factor_monat_im_jahr = $row->factor;
-
-    foreach ($Packungen as $time => $average) {
-        $Erwartung[$time] = $average * $factor_monat_im_jahr * $factor_tag_im_monat;
-    }
-//    echo '<pre>';    var_export($Packungen);   echo '</pre>';
-
-    return $Erwartung;
-}
-
 class roster_image_histogramm {
 
     function __construct() {
@@ -82,8 +29,8 @@ class roster_image_histogramm {
         $canvas_text = "ctx.beginPath();\n";
         $canvas_text .= "ctx.setLineDash([]);\n";
         $canvas_text .= "ctx.lineWidth=5;\n";
-        foreach ($Anwesende as $unix_time => $anwesende) {
-            $time_float = time_from_text_to_int(date('H:i:s', $unix_time));
+        foreach ($Anwesende as $time_in_seconds => $anwesende) {
+            $time_float = $time_in_seconds / 3600;
             $x_pos_line_start = $x_pos_line_end;
             $y_pos_line_start = $y_pos_line_end;
             $x_pos_line_end = ($time_float - $start_time) * $width_factor + $outer_margin_x;
@@ -108,10 +55,10 @@ class roster_image_histogramm {
     /**
      *
      * @param array $Roster
-     * @global int $branch_id
-     * @global string $datum
-     * @global array $Anwesende
-     * @global float $factor_employee The number of drug packages that can be sold per employee within a certain time.
+     * @param int $branch_id
+     * @param array $Anwesende
+     * @param string $datum
+     * @var float $factor_employee The number of drug packages that can be sold per employee within a certain time.
      * @return string The canvas element
      */
     public static function draw_image_histogramm($Roster, $branch_id, $Anwesende, $datum) {
@@ -119,8 +66,7 @@ class roster_image_histogramm {
         if (empty($Anwesende)) {
             return FALSE;
         }
-// We need the reader for Erwartung here or inside a seperate function file!
-        $Erwartung = get_Erwartung($datum, $branch_id);
+        $Erwartung = roster_image_histogramm::get_Erwartung($datum, $branch_id);
         $canvas_width = 650;
         $canvas_height = 300;
 
@@ -135,8 +81,8 @@ class roster_image_histogramm {
                 $End_times[] = $roster_item_object->duty_end_int;
             }
         }
-        $start_time = min($Start_times);
-        $end_time = max($End_times);
+        $start_time = min($Start_times) / 3600;
+        $end_time = max($End_times) / 3600;
         $duration = $end_time - $start_time;
         $width_factor = ($canvas_width - ($outer_margin_x * 2)) / $duration;
 
@@ -171,39 +117,89 @@ class roster_image_histogramm {
 
 
         $canvas_text .= "ctx.scale(" . 1 / $width_factor . ", " . 1 / $height_factor . ");\n";
-//        $canvas_text .= roster_image_histogramm::draw_image_dienstplan_add_headcount($outer_margin_x, $width_factor, $height_factor, $start_time, $Anwesende, $factor_employee);
+        $canvas_text .= roster_image_histogramm::draw_image_dienstplan_add_headcount($outer_margin_x, $width_factor, $height_factor, $start_time, $Anwesende, $factor_employee);
         //$canvas_text .= "ctx.strokeStyle = '#B4B4B4';";
         $canvas_text .= "ctx.strokeStyle = 'black';\n"; // = dot color
         $canvas_text .= "ctx.lineWidth=2;\n";
         $canvas_text .= "ctx.fillStyle = 'black';\n"; // = font color
         $canvas_text .= "ctx.font = '" . "$font_size" . "px sans-serif';\n";
         $canvas_text .= "ctx.textAlign = 'center';\n";
-        /*
-          for ($time = floor($start_time); $time <= ceil($end_time); $time = $time + 2) {
-          $x_pos = ($time - $start_time) * $width_factor + $outer_margin_x;
-          $x_pos_secondary = $x_pos + 1 * $width_factor;
-          $y_pos = 0;
-          $y_pos_line_start = (($outer_margin_y / $height_factor) + $font_size) * -1;
-          $y_pos_line_end = -$canvas_height + ($outer_margin_y / $height_factor);
-          $canvas_text .= "ctx.fillText('$time:00', '$x_pos', '$y_pos');\n";
-          $canvas_text .= "ctx.beginPath();\n"
-          . "ctx.setLineDash([1, 8]);\n"
-          . "ctx.moveTo($x_pos, $y_pos_line_start);\n"
-          . "ctx.lineTo($x_pos, $y_pos_line_end);\n"
-          . "ctx.stroke();\n"
-          . "ctx.closePath();\n";
-          $canvas_text .= "ctx.beginPath();\n"
-          . "ctx.setLineDash([1, 16]);\n"
-          . "ctx.moveTo($x_pos_secondary, $y_pos_line_start);\n"
-          . "ctx.lineTo($x_pos_secondary, $y_pos_line_end);\n"
-          . "ctx.stroke();\n"
-          . "ctx.closePath();\n";
-          } */
+        for ($time = floor($start_time); $time <= ceil($end_time); $time = $time + 2) {
+            $x_pos = ($time - $start_time) * $width_factor + $outer_margin_x;
+            $x_pos_secondary = $x_pos + 1 * $width_factor;
+            $y_pos = 0;
+            $y_pos_line_start = (($outer_margin_y / $height_factor) + $font_size) * -1;
+            $y_pos_line_end = -$canvas_height + ($outer_margin_y / $height_factor);
+            $canvas_text .= "ctx.fillText('$time:00', '$x_pos', '$y_pos');\n";
+            $canvas_text .= "ctx.beginPath();\n"
+                    . "ctx.setLineDash([1, 8]);\n"
+                    . "ctx.moveTo($x_pos, $y_pos_line_start);\n"
+                    . "ctx.lineTo($x_pos, $y_pos_line_end);\n"
+                    . "ctx.stroke();\n"
+                    . "ctx.closePath();\n";
+            $canvas_text .= "ctx.beginPath();\n"
+                    . "ctx.setLineDash([1, 16]);\n"
+                    . "ctx.moveTo($x_pos_secondary, $y_pos_line_start);\n"
+                    . "ctx.lineTo($x_pos_secondary, $y_pos_line_end);\n"
+                    . "ctx.stroke();\n"
+                    . "ctx.closePath();\n";
+        }
 
         $canvas_text .= "</script>";
-        print_debug_variable('$canvas_text', $canvas_text);
 //header("Content-type: image/canvas+xml");
         return $canvas_text;
     }
 
+    private static function get_Erwartung($datum, $branch_id) {
+        global $List_of_branch_objects;
+        /*
+          if (basename($_SERVER["SCRIPT_FILENAME"]) === 'tag-in.php') {
+          echo roster_image_histogramm::check_timeliness_of_pep_data();
+          }
+         */
+        $unix_datum = strtotime($datum);
+        $sql_weekday = date('N', $unix_datum) - 1;
+        $month_day = date('j', $unix_datum);
+        $month = date('n', $unix_datum);
+
+        $branch_pep_id = $List_of_branch_objects[$branch_id]->PEP;
+        if (empty($branch_pep_id)) {
+            return FALSE;
+        }
+
+        $result = mysqli_query_verbose("SELECT Uhrzeit, Mittelwert FROM `pep_weekday_time`  WHERE Mandant = $branch_pep_id and Wochentag = $sql_weekday");
+        while ($row = mysqli_fetch_object($result)) {
+            $Packungen[$row->Uhrzeit] = $row->Mittelwert;
+        }
+        $result = mysqli_query_verbose("SELECT factor FROM `pep_month_day`  WHERE `branch` = $branch_pep_id and `day` = $month_day");
+        $row = mysqli_fetch_object($result);
+        $factor_tag_im_monat = $row->factor;
+
+        $result = mysqli_query_verbose("SELECT factor FROM `pep_year_month`  WHERE `branch` = $branch_pep_id and `month` = $month");
+        $row = mysqli_fetch_object($result);
+        $factor_monat_im_jahr = $row->factor;
+
+        foreach ($Packungen as $time => $average) {
+            $Erwartung[$time] = $average * $factor_monat_im_jahr * $factor_tag_im_monat;
+        }
+        return $Erwartung;
+    }
+
+    /*
+      private static function check_timeliness_of_pep_data() {
+      //Check if the PEP information is still up-to-date:
+      $sql_query = "SELECT max(Datum) as Datum FROM `pep`";
+      $result = mysqli_query_verbose($sql_query);
+      $row = mysqli_fetch_object($result);
+      $newest_pep_date = strtotime($row->Datum);
+      $today = time();
+      $seconds_since_last_update = $today - $newest_pep_date;
+      if ($seconds_since_last_update >= 60 * 60 * 24 * 30 * 3) { //3 months
+      $timeliness_warning_html = "<br><div class=warningmsg>Die PEP Information ist veraltet. <br>"
+      . "Letzter Eintrag " . date('d.m.Y', strtotime($row->Datum)) . ". <br>"
+      . "Bitte neue PEP-Datei <a href=upload-in.php>hochladen</a>!</div><br>\n";
+      return $timeliness_warning_html;
+      }
+      }
+     */
 }
