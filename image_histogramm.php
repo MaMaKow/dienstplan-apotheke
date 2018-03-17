d01760a5b6a5204484118a57ff2e5d184beaee8d
ux31a <git-ux31a@martin-mandelkow.de>
2018-03-17 19:07:52 +0100

    image_histogram towards atomified functions in a class

--- image_histogramm.php
+++ image_histogramm.php
@@ -23,10 +23,64 @@ class roster_image_histogramm {
 
     }
 
+    private static function draw_image_dienstplan_add_Erwartung($outer_margin_x, $outer_margin_y, $width_factor, $height_factor, $start_time, $canvas_height, $Erwartung) {
+        $red = hex2rgb('#FF0000');
+        $x_start = $outer_margin_x / $width_factor;
+        $y_start = $outer_margin_y / $height_factor * -1;
+
+        $canvas_text .= "ctx.translate(0,$canvas_height);\n";
+        $canvas_text .= "ctx.scale($width_factor, $height_factor);\n";
+
+        $canvas_text .= "ctx.moveTo($x_start, $y_start);\n";
+        foreach ($Erwartung as $time => $packages) {
+            $x_pos = (time_from_text_to_int($time) - $start_time) + $outer_margin_x / $width_factor;
+            $y_pos = ($packages * -1) - ($outer_margin_y / $height_factor);
+            $canvas_text .= "ctx.lineTo($x_pos, $y_pos);\n";
+        }
+        //$canvas_text .= $canvas_box_text;
+        $canvas_text .= "ctx.lineTo($x_pos, $y_start);\n";
+        $canvas_text .= "ctx.closePath();\n";
+//    $canvas_text .= "ctx.stroke();\n";
+        $canvas_text .= "ctx.fillStyle = 'rgba($red, 0.5)';\n";
+        $canvas_text .= "ctx.fill();\n";
+        return $canvas_text;
+    }
+
+    private static function draw_image_dienstplan_add_axis_labeling($outer_margin_x, $outer_margin_y, $width_factor, $height_factor, $canvas_height, $start_time, $end_time) {
+        $font_size = 16;
+        $canvas_text .= "ctx.strokeStyle = 'black';\n"; // = dot color
+        $canvas_text .= "ctx.lineWidth=2;\n";
+        $canvas_text .= "ctx.fillStyle = 'black';\n"; // = font color
+        $canvas_text .= "ctx.font = '" . "$font_size" . "px sans-serif';\n";
+        $canvas_text .= "ctx.textAlign = 'center';\n";
+        for ($time = floor($start_time); $time <= ceil($end_time); $time = $time + 2) {
+            $x_pos = ($time - $start_time) * $width_factor + $outer_margin_x;
+            $x_pos_secondary = $x_pos + 1 * $width_factor;
+            $y_pos = 0;
+            $y_pos_line_start = (($outer_margin_y / $height_factor) + $font_size) * -1;
+            $y_pos_line_end = -$canvas_height + ($outer_margin_y / $height_factor);
+            $canvas_text .= "ctx.fillText('$time:00', '$x_pos', '$y_pos');\n";
+            $canvas_text .= "ctx.beginPath();\n"
+                    . "ctx.setLineDash([1, 8]);\n"
+                    . "ctx.moveTo($x_pos, $y_pos_line_start);\n"
+                    . "ctx.lineTo($x_pos, $y_pos_line_end);\n"
+                    . "ctx.stroke();\n"
+                    . "ctx.closePath();\n";
+            $canvas_text .= "ctx.beginPath();\n"
+                    . "ctx.setLineDash([1, 16]);\n"
+                    . "ctx.moveTo($x_pos_secondary, $y_pos_line_start);\n"
+                    . "ctx.lineTo($x_pos_secondary, $y_pos_line_end);\n"
+                    . "ctx.stroke();\n"
+                    . "ctx.closePath();\n";
+        }
+        return $canvas_text;
+    }
+
     private static function draw_image_dienstplan_add_headcount($outer_margin_x, $width_factor, $height_factor, $start_time, $Anwesende, $factor_employee) {
         $x_pos_line_end = 0;
         $y_pos_line_end = 0;
-        $canvas_text = "ctx.beginPath();\n";
+        $canvas_text .= "ctx.scale(" . 1 / $width_factor . ", " . 1 / $height_factor . ");\n";
+        $canvas_text .= "ctx.beginPath();\n";
         $canvas_text .= "ctx.setLineDash([]);\n";
         $canvas_text .= "ctx.lineWidth=5;\n";
         foreach ($Anwesende as $time_in_seconds => $anwesende) {
@@ -66,7 +120,6 @@ class roster_image_histogramm {
         if (empty($Anwesende)) {
             return FALSE;
         }
-        $Erwartung = roster_image_histogramm::get_Erwartung($datum, $branch_id);
         $canvas_width = 650;
         $canvas_height = 300;
 
@@ -74,7 +127,6 @@ class roster_image_histogramm {
 //    $inner_margin_y = $inner_margin_x;
         $outer_margin_x = 30;
         $outer_margin_y = 20;
-        $font_size = 16;
         foreach ($Roster as $Roster_day) {
             foreach ($Roster_day as $roster_item_object) {
                 $Start_times[] = $roster_item_object->duty_start_int;
@@ -85,67 +137,24 @@ class roster_image_histogramm {
         $end_time = max($End_times) / 3600;
         $duration = $end_time - $start_time;
         $width_factor = ($canvas_width - ($outer_margin_x * 2)) / $duration;
+        $Erwartung = roster_image_histogramm::get_Erwartung($datum, $branch_id);
 
         $max_work_load = max($Erwartung);
         $max_workforce = max($Anwesende) * $factor_employee;
         $max_height = max($max_work_load, $max_workforce);
         $height_factor = ($canvas_height - ($outer_margin_y * 2)) / $max_height;
 
-        $x_start = $outer_margin_x / $width_factor;
-        $y_start = $outer_margin_y / $height_factor * -1;
 
         $canvas_text = "<canvas id='canvas_histogram' width='$canvas_width' height='$canvas_height' >\n Your browser does not support the HTML5 canvas tag.\n </canvas>\n";
         $canvas_text .= "<script>\n";
         $canvas_text .= "var c = document.getElementById('canvas_histogram');\n";
         $canvas_text .= "var ctx = c.getContext('2d');\n";
-        $red = hex2rgb('#FF0000');
-        $canvas_text .= "ctx.translate(0,$canvas_height);\n";
-        $canvas_text .= "ctx.scale($width_factor, $height_factor);\n";
 
-        $canvas_text .= "ctx.moveTo($x_start, $y_start);\n";
-        foreach ($Erwartung as $time => $packages) {
-            $x_pos = (time_from_text_to_int($time) - $start_time) + $outer_margin_x / $width_factor;
-            $y_pos = ($packages * -1) - ($outer_margin_y / $height_factor);
-            $canvas_text .= "ctx.lineTo($x_pos, $y_pos);\n";
-        }
-        //$canvas_text .= $canvas_box_text;
-        $canvas_text .= "ctx.lineTo($x_pos, $y_start);\n";
-        $canvas_text .= "ctx.closePath();\n";
-//    $canvas_text .= "ctx.stroke();\n";
-        $canvas_text .= "ctx.fillStyle = 'rgba($red, 0.5)';\n";
-        $canvas_text .= "ctx.fill();\n";
-
-
-        $canvas_text .= "ctx.scale(" . 1 / $width_factor . ", " . 1 / $height_factor . ");\n";
+        $canvas_text .= roster_image_histogramm::draw_image_dienstplan_add_Erwartung($outer_margin_x, $outer_margin_y, $width_factor, $height_factor, $start_time, $canvas_height, $Erwartung);
         $canvas_text .= roster_image_histogramm::draw_image_dienstplan_add_headcount($outer_margin_x, $width_factor, $height_factor, $start_time, $Anwesende, $factor_employee);
-        //$canvas_text .= "ctx.strokeStyle = '#B4B4B4';";
-        $canvas_text .= "ctx.strokeStyle = 'black';\n"; // = dot color
-        $canvas_text .= "ctx.lineWidth=2;\n";
-        $canvas_text .= "ctx.fillStyle = 'black';\n"; // = font color
-        $canvas_text .= "ctx.font = '" . "$font_size" . "px sans-serif';\n";
-        $canvas_text .= "ctx.textAlign = 'center';\n";
-        for ($time = floor($start_time); $time <= ceil($end_time); $time = $time + 2) {
-            $x_pos = ($time - $start_time) * $width_factor + $outer_margin_x;
-            $x_pos_secondary = $x_pos + 1 * $width_factor;
-            $y_pos = 0;
-            $y_pos_line_start = (($outer_margin_y / $height_factor) + $font_size) * -1;
-            $y_pos_line_end = -$canvas_height + ($outer_margin_y / $height_factor);
-            $canvas_text .= "ctx.fillText('$time:00', '$x_pos', '$y_pos');\n";
-            $canvas_text .= "ctx.beginPath();\n"
-                    . "ctx.setLineDash([1, 8]);\n"
-                    . "ctx.moveTo($x_pos, $y_pos_line_start);\n"
-                    . "ctx.lineTo($x_pos, $y_pos_line_end);\n"
-                    . "ctx.stroke();\n"
-                    . "ctx.closePath();\n";
-            $canvas_text .= "ctx.beginPath();\n"
-                    . "ctx.setLineDash([1, 16]);\n"
-                    . "ctx.moveTo($x_pos_secondary, $y_pos_line_start);\n"
-                    . "ctx.lineTo($x_pos_secondary, $y_pos_line_end);\n"
-                    . "ctx.stroke();\n"
-                    . "ctx.closePath();\n";
-        }
+        $canvas_text .= roster_image_histogramm::draw_image_dienstplan_add_axis_labeling($outer_margin_x, $outer_margin_y, $width_factor, $height_factor, $canvas_height, $start_time, $end_time);
 
-        $canvas_text .= "</script>";
+        $canvas_text .= "</script>\n";
 //header("Content-type: image/canvas+xml");
         return $canvas_text;
     }
