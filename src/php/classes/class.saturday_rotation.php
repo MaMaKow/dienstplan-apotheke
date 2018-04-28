c4d495021ce8f34e8b5fa5766ac9b64a12f2343a
Dr. Martin Mandelkow <MaMaKow@users.noreply.github.com>
2018-04-28 16:00:03 +0200

    create class saturday_rotation

---
+++ src/php/classes/class.saturday_rotation.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright (C) 2018 Dr. rer. nat. M. Mandelkow <netbeans-pdr@martin-mandelkow.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This class provides the option to assign a specific shift in a rotation.
+ *
+ * The shift will be a specific shift on saturday.
+ * The rotation will use a predefined team.
+ * This class does not take into consideration the absence of employees from the teams.
+ * The function examine_attendance::check_for_attendant_absentees() will however create a warning/error.
+ *
+ * @author Dr. rer. nat. M. Mandelkow <netbeans-pdr@martin-mandelkow.de>
+ */
+class saturday_rotation {
+
+}
