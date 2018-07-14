eec8cdf16f0486e63206bf6462b743150fd5cbf6
ux31a <git-ux31a@martin-mandelkow.de>
2018-07-14 23:51:02 +0200

    bugfix: git not working inside .git directory

--- tests/create_testlog.sh
+++ tests/create_testlog.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #This script will create a list of tests to be done for the current commit.
 root_folder=$(git rev-parse --show-toplevel);
+branch_name=$(git symbolic-ref --short HEAD);
+commit_name=$(git describe);
 echo "Changing directory to $root_folder";
 cd "$root_folder/tests"; #will only cd if we are above tests
 mkdir -p "$root_folder/tests/log";
 
-branch_name=$(git symbolic-ref --short HEAD);
 if [ "testing" == "$branch_name" ]
 then
     echo "We are on the testing branch. A test log file will be created.";
-    commit_name=$(git describe);
     target_file_name="$root_folder/tests/log/tests_for_$commit_name";
     if [ ! -f $target_file_name ]
     then
