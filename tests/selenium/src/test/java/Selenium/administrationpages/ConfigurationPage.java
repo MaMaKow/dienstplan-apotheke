8c8b2f055383a94a888c9907142a1ea9f8b5e9a1
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2021-09-19 12:25:46 +0200

    Selenium test configuration page

--- tests/selenium/src/test/java/Selenium/administrationpages/ConfigurationPage.java
+++ tests/selenium/src/test/java/Selenium/administrationpages/ConfigurationPage.java
@@ -23,6 +23,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -63,10 +64,139 @@ public class ConfigurationPage {
         return databaseUserElement.getAttribute("value");
     }
 
+    public String getDatabasePassword() {
+        /**
+         * The password MUST NOT be visible!
+         */
+        By databaseUserBy = By.xpath("/html/body/div[3]/form/div/fieldset[1]/input[@name='database_password']");
+        WebElement databaseUserElement = driver.findElement(databaseUserBy);
+        return databaseUserElement.getAttribute("value");
+    }
+
     public String getContactEmail() {
-        By databasePasswordBy = By.xpath("/html/body/div[3]/form/div/fieldset[2]/input[@name='contact_email']");
-        WebElement databasePasswordElement = driver.findElement(databasePasswordBy);
-        return databasePasswordElement.getAttribute("value");
+        By elementBy = By.xpath("/html/body/div[3]/form/div/fieldset[2]/input[@name='contact_email']");
+        WebElement webElement = driver.findElement(elementBy);
+        return webElement.getAttribute("value");
+    }
+
+    public String getLanguage() {
+        By elementBy = By.xpath("/html/body/div[3]/form/div/fieldset[3]/select[@name='language']");
+        WebElement webElement = driver.findElement(elementBy);
+        Select selectElement = new Select(webElement);
+        return selectElement.getFirstSelectedOption().getText();
+
+    }
+
+    public String getLocales() {
+        By elementBy = By.xpath("/html/body/div[3]/form/div/fieldset[3]/input[@name='LC_TIME']");
+        WebElement webElement = driver.findElement(elementBy);
+        return webElement.getAttribute("value");
+    }
+
+    public String getEncoding() {
+        By elementBy = By.xpath("/html/body/div[3]/form/div/fieldset[3]/input[@name='mb_internal_encoding']");
+        WebElement webElement = driver.findElement(elementBy);
+        return webElement.getAttribute("value");
+    }
+
+    public Integer getErrorReporting() {
+        Integer errorReportingValue = null;
+        /**
+         * By xpaths:
+         */
+        By errorReportingErrorBy = By.xpath("//*[@id=\"error_reporting_error\"]");
+        By errorReportingWarningBy = By.xpath("//*[@id=\"error_reporting_warning\"]");
+        By errorReportingNoticeBy = By.xpath("//*[@id=\"error_reporting_notice\"]");
+        By errorReportingAllBy = By.xpath("//*[@id=\"error_reporting_all\"]");
+        /**
+         * WebElements:
+         */
+        WebElement errorReportingErrorElement = driver.findElement(errorReportingErrorBy);
+        WebElement errorReportingWarningElement = driver.findElement(errorReportingWarningBy);
+        WebElement errorReportingNoticeElement = driver.findElement(errorReportingNoticeBy);
+        WebElement errorReportingAllElement = driver.findElement(errorReportingAllBy);
+        /**
+         * Only one radio can be selected:
+         */
+        if (errorReportingErrorElement.isSelected()) {
+            errorReportingValue = Integer.valueOf(errorReportingErrorElement.getAttribute("value")); //4437
+            return errorReportingValue;
+        }
+        if (errorReportingWarningElement.isSelected()) {
+            errorReportingValue = Integer.valueOf(errorReportingWarningElement.getAttribute("value")); //5111
+            return errorReportingValue;
+        }
+        if (errorReportingNoticeElement.isSelected()) {
+            errorReportingValue = Integer.valueOf(errorReportingNoticeElement.getAttribute("value")); //30719
+            return errorReportingValue;
+        }
+        if (errorReportingAllElement.isSelected()) {
+            errorReportingValue = Integer.valueOf(errorReportingAllElement.getAttribute("value")); //32767
+            return errorReportingValue;
+        }
+        /**
+         * found none:
+         */
+        return errorReportingValue;
+    }
+
+    public boolean getHideDisapproved() {
+        Boolean hideDisapprovedValue = null;
+        /**
+         * By xpaths:
+         */
+        By showDisapprovedBy = By.xpath("//*[@id=\"configuration_input_div\"]/fieldset[5]/input[@value=\"0\"]");
+        By hideDisapprovedBy = By.xpath("//*[@id=\"configuration_input_div\"]/fieldset[5]/input[@value=\"1\"]");
+        /**
+         * WebElements:
+         */
+        WebElement showDisapprovedElement = driver.findElement(showDisapprovedBy);
+        WebElement hideDisapprovedElement = driver.findElement(hideDisapprovedBy);
+        /**
+         * Only one radio can be selected:
+         */
+        if (showDisapprovedElement.isSelected()) {
+            hideDisapprovedValue = false;
+            return hideDisapprovedValue;
+        }
+        if (hideDisapprovedElement.isSelected()) {
+            hideDisapprovedValue = true;
+            return hideDisapprovedValue;
+        }
+        /**
+         * found none:
+         */
+        return hideDisapprovedValue;
+    }
+
+    public String getEmailMethod() {
+        String hideDisapprovedValue = null;
+        /**
+         * By selector: The selected element is :checked
+         */
+        //By showDisapprovedBy = By.cssSelector("#configuration_input_div > fieldset:nth-child(7) > input[type=radio]:nth-child(3)");
+        By emailMethodBy = By.cssSelector("#configuration_input_div > fieldset:nth-child(7) > input[type=radio]:checked");
+        /**
+         * WebElements:
+         */
+        WebElement emailMethodElement = driver.findElement(emailMethodBy);
+        /**
+         * Only one radio can be selected:
+         */
+        if (emailMethodElement.isSelected()) {
+            hideDisapprovedValue = emailMethodElement.getAttribute("value");
+            return hideDisapprovedValue;
+        }
+        /**
+         * found none:
+         */
+        return hideDisapprovedValue;
+    }
+
+    public void submitForm() {
+        By submitButtonBy = By.xpath("//*[@id=\"configuration_input_div\"]/input[@type=\"submit\"]");
+        WebElement submitButtonElement = driver.findElement(submitButtonBy);
+        submitButtonElement.click();
     }
 
     /**
