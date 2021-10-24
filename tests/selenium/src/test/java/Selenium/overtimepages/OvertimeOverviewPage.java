ae47fd8cde9be248539b3ab66eb91ed7cd788824
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2021-10-24 22:44:20 +0200

    Selenium test OvertimeOverviewPage

---
+++ tests/selenium/src/test/java/Selenium/overtimepages/OvertimeOverviewPage.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2021 Mandelkow
+ *
+ * Dienstplan Apotheke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package Selenium.overtimepages;
+
+import Selenium.MenuFragment;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author Mandelkow
+ */
+public class OvertimeOverviewPage {
+
+    protected static WebDriver driver;
+
+    public OvertimeOverviewPage(WebDriver driver) {
+        this.driver = driver;
+
+        if (this.getUserNameText().isEmpty()) {
+            throw new IllegalStateException(
+                    "This is not a logged in state," + " current page is: " + driver.getCurrentUrl());
+        }
+        MenuFragment.navigateTo(driver, MenuFragment.MenuLinkToOvertimeOverview);
+    }
+
+    /**
+     * Get user_name (span tag)
+     *
+     * We only need this in order to check, if we are logged in. TODO: Could
+     * this be a static method in the signInPage class?
+     *
+     * @return String user_name text
+     */
+    public String getUserNameText() {
+        final By userNameSpanBy = By.id("MenuListItemApplicationUsername");
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(userNameSpanBy));
+
+        return driver.findElement(userNameSpanBy).getText();
+    }
+
+    public Float getBalanceByEmployeeName(String nameString) {
+        By employeeBalanceColumnBy = By.xpath("/html/body/table/tbody/tr/td[contains(text(), '" + nameString + "')]/parent::tr/td[2]");
+        WebElement employeeBalanceColumnElement = driver.findElement(employeeBalanceColumnBy);
+        return Float.valueOf(employeeBalanceColumnElement.getText());
+    }
+
+}
---
+++ tests/selenium/src/test/java/Selenium/overtimepages/TestOvertimeOverviewPage.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2021 Mandelkow
+ *
+ * Dienstplan Apotheke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package Selenium.overtimepages;
+
+import Selenium.ReadPropertyFile;
+import Selenium.ScreenShot;
+import Selenium.signinpage.SignInPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Mandelkow
+ */
+/**
+ *
+ * @author Martin Mandelkow <netbeans@martin-mandelkow.de>
+ */
+public class TestOvertimeOverviewPage {
+
+    WebDriver driver;
+
+    @Test(enabled = true)/*new*/
+    public void testDisplay() {
+        driver = Selenium.driver.Wrapper.getDriver();
+        ReadPropertyFile readPropertyFile = new ReadPropertyFile();
+        String urlPageTest = readPropertyFile.getUrlPageTest();
+        driver.get(urlPageTest);
+
+        /**
+         * Sign in:
+         */
+        SignInPage signInPage = new SignInPage(driver);
+        String pdr_user_password = readPropertyFile.getPdrUserPassword();
+        String pdr_user_name = readPropertyFile.getPdrUserName();
+        signInPage.loginValidUser(pdr_user_name, pdr_user_password);
+        OvertimeOverviewPage overtimeOverviewPage = new OvertimeOverviewPage(driver);
+        Assert.assertEquals(overtimeOverviewPage.getUserNameText(), pdr_user_name);
+
+        /**
+         * Find the overtime balance:
+         */
+        Float balance = overtimeOverviewPage.getBalanceByEmployeeName("Mandelkow");
+        Assert.assertEquals(balance, (float) 70);
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        Selenium.driver.Wrapper.createNewDriver();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult testResult) {
+        driver = Selenium.driver.Wrapper.getDriver();
+        new ScreenShot(testResult);
+        if (testResult.getStatus() != ITestResult.FAILURE) {
+            driver.quit();
+        }
+    }
+
+}
