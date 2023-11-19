292b4026656f629093cc3d492bd045a457874940
Dr. Martin Mandelkow <github@martin-mandelkow.de>
2023-11-19 22:12:04 +0100

    Refactor commit helper to not tag on feature branches.

--- tests/commit-helper.sh
+++ tests/commit-helper.sh
@@ -10,59 +10,70 @@ current_version_major=`echo $current_version | cut -d. -f1 -`;
 current_version_minor=`echo $current_version | cut -d. -f2 -`;
 current_version_patch=`echo $current_version | cut -d. -f3 - | cut -d- -f1 -`;
 
+# Get the current branch name:
+current_branch=$(git symbolic-ref --short HEAD)
+
 # Display information about the current state:
 clear;
 echo "We are in the directory"
 pwd
 echo "We are currently on the commit $current_version.";
 echo "major: $current_version_major";
-echo "minor $current_version_minor";
-echo "patch $current_version_patch";
+echo "minor: $current_version_minor";
+echo "patch: $current_version_patch";
 
 # echo "Writting current state of the database structure into the src/sql/ folder";
 # php "tests\get-database-structure.php";
 
 echo "";
-echo "Showing git status: "; 
+echo "Showing git status: ";
 git status;
 
 
 
-# Determine the correct tag for this commit:
-echo "";
-read -p "Will this commit be tagged as a new MAJOR version? [y/n] " -N 1 decision_git_tag_major;
-if [ "y" == "$decision_git_tag_major" ] || [ "Y" == "$decision_git_tag_major" ]
+# Check if the current branch is one of the allowed branches for tagging:
+if [ "$current_branch" != "development" ] && [ "$current_branch" != "testing" ] && [ "$current_branch" != "master" ]
 then
-    # We start a new major branch.
-    # This automatically means, that the minor version and the patch version are set to 0.
-    new_version_major=$(($current_version_major + 1));
-    new_version_minor=0;
-    new_version_patch=0;
+    featureBranch=true;
+    echo "$current_branch probably is a feature branch: We will only tag commits on the 'development,' 'testing,' or 'master' branches."
 else
-    # We keep the old number:
-    new_version_major=$current_version_major;
-
-    # But perhaps the minor version changed?
+    featureBranch=false;
+    # Determine the correct tag for this commit:
     echo "";
-    read -p "Will this commit be tagged as a new Minor version? [y/n] " -N 1 decision_git_tag_minor;
-    if [ "y" == "$decision_git_tag_minor" ] || [ "Y" == "$decision_git_tag_minor" ]
+    read -p "Will this commit be tagged as a new MAJOR version? [y/n] " -N 1 decision_git_tag_major;
+    if [ "y" == "$decision_git_tag_major" ] || [ "Y" == "$decision_git_tag_major" ]
     then
-        new_version_minor=$(($current_version_minor + 1));
+        # We start a new major branch.
+        # This automatically means, that the minor version and the patch version are set to 0.
+        new_version_major=$(($current_version_major + 1));
+        new_version_minor=0;
         new_version_patch=0;
     else
-        # We keep the old number
-        new_version_minor=$current_version_minor;
-        new_version_patch=$(($current_version_patch + 1));
+        # We keep the old number:
+        new_version_major=$current_version_major;
+
+        # But perhaps the minor version changed?
+        echo "";
+        read -p "Will this commit be tagged as a new Minor version? [y/n] " -N 1 decision_git_tag_minor;
+        if [ "y" == "$decision_git_tag_minor" ] || [ "Y" == "$decision_git_tag_minor" ]
+        then
+            new_version_minor=$(($current_version_minor + 1));
+            new_version_patch=0;
+        else
+            # We keep the old number
+            new_version_minor=$current_version_minor;
+            new_version_patch=$(($current_version_patch + 1));
+        fi
     fi
+    new_version=$new_version_major.$new_version_minor.$new_version_patch;
+    echo "";
+    echo "The new version number will be $new_version";
+    sed -i 's/<span id="pdrVersionSpan">[0-9.]*<\/span>/<span id="pdrVersionSpan">'$new_version'<\/span>/' src/php/pages/about.php
+    git add src/php/pages/about.php
+    sed -i '/<artifactId>selenium</,/<name>SeleniumTest</ s/\(<version>\).*\(<\/version>\)/\1'$new_version'\2/' tests/selenium/pom.xml
+    git add tests/selenium/pom.xml
 fi
 
-new_version=$new_version_major.$new_version_minor.$new_version_patch;
-echo "";
-echo "The new version number will be $new_version";
-sed -i 's/<span id="pdrVersionSpan">[0-9.]*<\/span>/<span id="pdrVersionSpan">'$new_version'<\/span>/' src/php/pages/about.php
-git add src/php/pages/about.php
-sed -i '/<artifactId>selenium</,/<name>SeleniumTest</ s/\(<version>\).*\(<\/version>\)/\1'$new_version'\2/' tests/selenium/pom.xml
-git add tests/selenium/pom.xml
 # TODO: <p lang=de>Ich würde hier sehr gerne das script Tests\get-database-structure.php laufen lassen.
 # Dabei gibt es allerdings ein Problem.
 # Eine Entwicklungsumgebung hat nicht zwingend Zugriff auf eine Datenbank. Sie kann also nicht immer ihre eigene Datenbankstruktur besitzen.
@@ -85,11 +96,15 @@ sed -i 's/\(#:[[:space:]]*.*:\)[0-9]*$/\1/' "$po_file"
 echo "In Kommentaren wurden Zeilenangaben aus der $po_file-Datei entfernt."
 git add "$po_file"
 
-read -p "Ready to COMMIT and sign the changes? [y/n] " -N 1 decision_commit;
+read -p "Ready to CREATE commit message, COMMIT and sign the changes? [y/n] " -N 1 decision_commit;
 if [ "y" == "$decision_commit" ] || [ "Y" == "$decision_commit" ]
 then
     clear
-    git commit --gpg-sign && git tag "$new_version"
+    git commit --gpg-sign
+    if [ true == $featureBranch ]
+    then
+        git tag "$new_version"
+    fi
     git show -1
     git status
     read -p "Ready to PUSH changes and tags to remote? [y/n] " -N 1 decision_push;
